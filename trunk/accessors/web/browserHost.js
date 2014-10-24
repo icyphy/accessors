6459028613ee5b6978fd7c1fc6cca682f6ce3dcc
eal <eal@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2014-10-24 00:20:30 +0000

    Made the Hue accessor work (partially) with the Browser host

--- trunk/accessors/web/browserHost.js
+++ trunk/accessors/web/browserHost.js
@@ -38,3 +38,21 @@ function readURL(url) {
         throw "readURL did not complete: " + url;
     }
 }
+// Method for performing an HTTP request.
+function httpRequest(url, method, properties, body, timeout) {
+    var request = new XMLHttpRequest();
+    // The third argument specifies a synchronous read.
+    request.open(method, url, false);
+    // Null argument says there is no body.
+    request.send(body);
+    // readyState === 4 is the same as readyState === request.DONE.
+    if (request.readyState === request.DONE) {
+        if (request.status <= 400) {
+            return request.responseText;
+        } else {
+            throw "httpRequest failed with code " + request.status + " at URL: " + url;
+        }
+    } else {
+        throw "httpRequest did not complete: " + url;
+    }
+}    
