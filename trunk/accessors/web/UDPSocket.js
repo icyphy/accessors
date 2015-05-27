9f212af211af6de94a8e71139138030b26ea244f
hokeunkim <hokeunkim@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2015-05-27 22:36:03 +0000

    Added a preliminary version of the UDP socket.

---
+++ trunk/accessors/web/UDPSocket.js
@@ -0,0 +1,45 @@
+// This accessor requires the optional 'udpSocket' module, which may or may
+// not be provided by an accessor host. Most hosts will provide this module.
+var UDPSocket = require('udpSocket');
+
+// Set up the accessor. In an XML specification, this information would
+// be provided in XML syntax.
+exports.setup = function() {
+    accessor.author('Hokeun Kim');
+    accessor.version('0.1 $Date$');
+    accessor.input('port', {
+        'value':'22000',
+        'type':'number',
+        'description':'The port number.'
+    });
+    accessor.output('received', {
+        'type':'string',
+        'description':'The received string.'
+    });
+    accessor.description(
+        'This accessor listens to the UDP port.',
+        'text/html'
+    );
+};
+
+// Define the functionality.
+function onMessage(message) {
+    console.log('Received from web socket: ' + message);
+    send(message, 'received');
+}
+
+var socket = null;
+
+exports.initialize = function() {
+    // Invoke the getPrice function each time a 'symbol' input arrives.
+    socket = UDPSocket.createSocket();
+    var port = get('port');
+    socket.bind(port);
+}
+
+exports.wrapup = function() {
+    // Failing to do this will likely trigger an exception when the model stops running,
+    // because the getPrice() function will attempt to send an output after the model
+    // has stopped.
+    socket.close();
+}
