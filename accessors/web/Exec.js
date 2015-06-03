8e192aeea8910986946a28bcd2c4475dd20faf61
Armin Wasicek <4086213+arminwasicek@users.noreply.github.com>
2015-06-03 04:14:31 +0000

    Added Exec accessor to call a shell

---
+++ accessors/web/Exec.js
@@ -0,0 +1,72 @@
+
+/** Exec starts a shell command and connects to the process' stdin and stdout.
+ * This accessor implements an interface to the shell of the host. It takes a
+ * command and starts it as a process. It connects the process' stdin and 
+ * stdout to the accessor ports with the same name.
+ * FIXME: Provide a selection of shells (e.g., bash, awk, etc)
+ *
+ *  @accessor Exec
+ *  @module Exec
+ *  @author Armin Wasicek (arminw@berkeley.edu)
+ *  @stdin The stdin of the executing process. A token received on this
+ *        port is interpreted as a line entered in stdin of the process.
+ *  @stdout The stdout of the executing process. Each line read from the 
+ *        executing process is sent out as a token from this port.
+ *  @command The command to be executed.
+ */
+
+// This accessor requires the 'shell' module, which may or may
+// not be provided by an accessor host. As this is a very powerful 
+// module, not all host may provide this module for security reasons.
+var shell = require('shell/shell');
+
+/** Global object to the shell module provided by the host. */
+var sh = null;
+
+/** Set up the accessor by defining the inputs and outputs.
+ */
+exports.setup = function() {
+    accessor.author('Armin Wasicek');
+	accessor.input('stdin', {
+		'type': 'string',
+		'description': 'some input'
+	});
+	accessor.output('stdout', {
+		'type': 'string',
+		'description': 'some output'
+	});
+    accessor.input('command', {
+        'value': 'ls',
+        'type':'string',
+        'description':'The command to be executed.'
+    });
+}
+
+/** Initialize the accessor and start the process subsequently.
+ */
+exports.initialize = function() {	
+	sh = new shell.Shell({'cmd' : get('command')});
+	
+	addInputHandler('stdin', function() {
+	    var data = get('stdin');
+	    if (data) {
+		  sh.write(data);
+	    }
+	});
+
+	sh.on('message', function(data) {
+		if(data)  {
+			send('stdout', data.toString());
+		}		
+	});
+
+	sh.start();
+}
+	
+/** Wrap up the execution of the accessor by stopping the process.
+ */
+exports.wrapup = function() {
+	if(sh)  {
+		sh.wrapup();
+	}
+}
