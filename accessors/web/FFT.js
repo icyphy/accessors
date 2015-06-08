7ecf4f09e26ed50fe7ffbc4e419368ef1029b1fe
Ilge Akkaya <3813409+ilge@users.noreply.github.com>
2015-06-08 22:10:41 +0000

    audio capture

--- accessors/web/FFT.js
+++ accessors/web/FFT.js
@@ -1,6 +1,34 @@
-// Accessor to return an array of FFT coefficients, given an input array   
+// Copyright (c) 2014-2015 The Regents of the University of California.
+// All rights reserved.
 
-// Set up the accessor.
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+
+/** This accessor returns an array of FFT coefficients, corresponding to the input signal.
+ *
+ *  @accessor FFT
+ *  @module dsp
+ *  @author Ilge Akkaya 
+ *  @version 0.1 $ID$
+ *  @input {[number]} signalIn Input signal array
+ *  @output {[{'real':'number', 'imag': 'number'}]} fftOutput An array of FFT coefficients, each being a record with real and imaginary parts.
+ */
 exports.setup = function() {
     accessor.author('FIXME');
     accessor.input('signalIn');
