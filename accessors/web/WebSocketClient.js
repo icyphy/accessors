9e041150b7464a822a1d5b3afdffaa10bc66bc1a
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
2015-06-27 17:22:01 +0000

    Updates to WebSocket: - Avoid registering a new input handler on every retry or every close. - Avoid reconnecting (on close or just a retry) if wrapup has been called. - Removed spurious port argument to removeInputHandler(). - If a connection succeeds, and wrapup() has been called, then immediately disconnect. - Unregister handlers if reconnectOnClose == false and remote side disconnects. - Print warning messages about any unfulfilled send requests when wrapup is called. - Added discardMessagesBeforeOpen parameter to WebSocketClient. - Improved the documentation of WebSocketClient. - Added throttleFactor parameter to WebSocketClient.

--- accessors/web/WebSocketClient.js
+++ accessors/web/WebSocketClient.js
@@ -25,12 +25,19 @@
  *  begins connecting to the web socket server.
  *  Once the connection is established, a `true` boolean is sent to
  *  the `connected` output.
- *
  *  If connection is not established immediately, the accessor will attempt to
  *  reconnect (numberOfRetries) times at an interval of (reconnectInterval)
  *
- *  Whenever an input is received on the `'toSend'`
- *  input, the message is sent to the socket.
+ *  Whenever an input is received on the `toSend`
+ *  input, the message is sent to the socket. If the socket is not yet open,
+ *  this accessor will, by default, queue the message to send when the socket opens,
+ *  unless the `discardMessagesBeforeOpen` parameter is true, in which case,
+ *  input messages that are received before the socket is opened will be
+ *  discarded. If messages are queued and `throttleFactor` is non-zero, then
+ *  whenever a message is queued to be later sent, the accessor will stall
+ *  by a number of milliseconds given by the queue size times the throttleFactor.
+ *  The longer the queue, the longer the stall. Note that this will likely block
+ *  the host from executing, so this feature should be used with caution.
  *
  *  Whenever a message is received from the socket, that message is
  *  produced on the `'received'` output.
@@ -43,19 +50,40 @@
  *
  *  The data can be any type that has a JSON representation.
  *  For incoming messages, this accessor assumes that the message is
- *  a string in UTF-8 that encodes a JSON object.<br/>
+ *  a string in UTF-8 that encodes a JSON object.
  *
- *  This accessor can be extended by other accessors that need web socket functionality. 
- *  See `RosPublisher.js` for an example.
+ *  When a model with an instance of this accessor stops executing, there
+ *  are two mechanisms by which data in transit can be lost. In both cases, warning
+ *  messages or error messages will be issued to the host to be displayed or otherwise
+ *  handled as the host sees fit.
+ *  
+ *  # First, there might be queued messages that were received on `toSend` but have not yet
+ *    been sent, either because the socket has not yet been opened or because
+ *    it was closed from the other side.
+ *  # Second, a message might be received from the server after shutdown has commenced.
+ *    In particular, received messages are handled asynchronously by a handler function
+ *    that can be invoked at any time, and that handler might be invoked after it is no
+ *    longer possible for this accessor to produce outputs (it has entered its wrapup
+ *    phase of execution).
+ *  
+ *  The server might similarly lose messages by the same two mechanisms occurring
+ *  on the server side. In that case, messages will presumably be displayed on the
+ *  server side.
+ *
+ *  Accessors that extend this one can override the `toSendInputHandler` function
+ *  to customize what is sent. See `RosPublisher.js` for an example.
  *
  *  This accessor requires the 'webSocket' module.
  *
  *  @accessor WebSocketClient
+ *  @module WebSocketClient
  *  @parameter {string} server The IP address or domain name of server. Defaults to 'localhost'.
  *  @parameter {int} port The port that the web socket listens to. Defaults to 8080.
  *  @parameter {int} numberOfRetries The number of times to retry if a connection fails. Defaults to 5.
  *  @parameter {int} timeBetweenRetries The time between retries in milliseconds. Defaults to 100.
- *  @parameter {boolean} reconnectOnClose The option of whether or not to reconnect when disconnected. 
+ *  @parameter {boolean} reconnectOnClose The option of whether or not to reconnect when disconnected.
+ *  @parameter {boolean} discardMessagesBeforeOpen If true, then any messages received on `toSend` before the socket is open will be discarded. This defaults to false.
+ *  @parameter {int} throttleFactor If non-zero, specifies a time (in milliseconds) to stall when a message is queued because the socket is not yet open. The time of the stall will be the queue size (after adding the message) times the throttleFactor. This defaults to 0.
  *  @input {JSON} toSend The data to be sent over the socket.
  *  @output {boolean} connected Output `true` on connected and `false` on disconnected.
  *  @output {JSON} received The data received from the web socket server.
@@ -89,6 +117,14 @@ exports.setup = function() {
     type: 'boolean',
     value: true
   });
+  parameter('discardMessagesBeforeOpen', {
+    type: 'boolean',
+    value: false
+  });
+  parameter('throttleFactor', {
+    type: 'int',
+    value: 0
+  });
   input('toSend', {
     type: 'JSON'
   });
@@ -108,7 +144,9 @@ exports.initialize = function() {
     'host':getParameter('server'),
     'port':getParameter('port'),
     'numberOfRetries':getParameter('numberOfRetries'),
-    'timeBetweenRetries':getParameter('timeBetweenRetries')
+    'timeBetweenRetries':getParameter('timeBetweenRetries'),
+    'discardMessagesBeforeOpen':getParameter('discardMessagesBeforeOpen'),
+    'throttleFactor':getParameter('throttleFactor')
   });
   
   client.on('open', onOpen);
@@ -146,36 +184,51 @@ function onOpen() {
    send('connected', true);
 }
   
-/** Executes once web socket closes.<br>
- *  Sets 'connected' output to false.<br>
- *  If reconnect parameter is true, call initialize() of caller's object 
- *  (this could be a derived accessor).
+/** Send false to 'connected' output, and if 'reconnectOnClose'
+ *  parameter evaluates to true and wrapup() has not been called,
+ *  then invoke initialize().
+ *  This will be called if either side closes the connection.
+ *  @param message Possible message about the closure.
  */
 function onClose(message) {
-   send('connected', false);
-   console.log('Status: Connection closed: ' + message);
-   if (getParameter('reconnectOnClose')) {
+  console.log('Status: Connection closed: ' + message);
+  if (inputHandle) {
+    // wrapup() has not been called.
+    // Probably the server closed the connection.
+    send('connected', false);
+    // Reconnect if reconnectOnClose is true.
+    if (getParameter('reconnectOnClose')) {
+      // Use 'this' rather than 'export' so initialize() can be overridden.
       this.initialize();
-   }
+    } else {
+      // Not set to reconnect on close.
+      // Close and unregister everything.
+      client.removeAllListeners('open');
+      client.removeAllListeners('message');
+      client.removeAllListeners('close');
+      client = null;
+    }
+  }
 }
   
-/** Outputs message received from web socket. */
+/** Send the message received from web socket to the 'received' output. */
 function onMessage(message) {
    send('received', message);
 }
   
-/** Closes web socket connection. */
+/** Close the web socket connection. */
 exports.wrapup = function() {
-   if (inputHandle != null) {
-      removeInputHandler(inputHandle, 'toSend');
-   }
-   //'error' event may be triggered on wrapup, so don't remove listener
-   if (client) {
-      client.removeAllListeners('open');
-      client.removeAllListeners('message');
-      client.removeAllListeners('close');
-      console.log('closing listeners');
-      client.close();
-   }
+  if (inputHandle != null) {
+    removeInputHandler(inputHandle);
+    inputHandle = null;
+  }
+  if (client) {
+    client.removeAllListeners('open');
+    client.removeAllListeners('message');
+    client.removeAllListeners('close');
+    client.close();
+    console.log('Status: Connection closed in wrapup.');
+    client = null;
+  }
 }
 
