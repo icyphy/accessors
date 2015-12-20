75d627f7c50bf89cc9cbfeaabeac0b52f2c89639
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
2015-12-20 18:37:07 +0000

    Handle difference in paths between accessors/web/... on the test server and accessor/... on the terraswarm server

--- accessors/web/hosts/browser/test/testServer.js
+++ accessors/web/hosts/browser/test/testServer.js
@@ -19,6 +19,18 @@ server.on('request', function(request, response) {
         while (url.substring(0,1) == '/') {
             url = url.substring(1);
         }
+        // Strip leading 'accessors/'.
+        // This is because the terraswarm.org server serves pages with paths like:
+        //    '/accessors/hosts/common/test/TestAccessor.js'
+        // but the actual file location is:
+        //    '/accessors/web/hosts/common/test/TestAccessor
+        // So to make the code work both in this test server and in the terraswarm
+        // server, we assume this server is executed in the directory
+        //    '/accessors/web'
+        // and hence the path should not include any leading '/accessors/'.
+        if (url.indexOf('accessors/') == 0) {
+            url = url.substring(10);
+        }
         // Disallow any ..
         if (url.indexOf('..') < 0) {
             fs.readFile(url, 'utf-8', function(error, data) {
