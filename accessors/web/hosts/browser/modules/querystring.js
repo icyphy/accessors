cfdc86abe6ec8124f1ecd1d27faaaec53dd176c9
beth <21232697+bethosyk@users.noreply.github.com>
2016-01-22 00:46:54 +0000

    httpClient implementation for browser host

---
+++ accessors/web/hosts/browser/modules/querystring.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Accessors:  Changed path to use modules directory instead of ./
+//exports.decode = exports.parse = require('./decode');
+//exports.encode = exports.stringify = require('./encode');
+
+exports.decode = exports.parse = require('decode');
+exports.encode = exports.stringify = require('encode');
+
