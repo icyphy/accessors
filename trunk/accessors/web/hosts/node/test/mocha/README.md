c35a77785d47ecd782275d7a98cedf49f349c3d3
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-02-11 01:43:05 +0000

    Added a start on using Mocha to test the node.js accessor host.

---
+++ trunk/accessors/web/hosts/node/test/mocha/README.md
@@ -0,0 +1,16 @@
+Version: $Id$
+
+How to run the Accessors Node.js tests using Mocha.
+
+1) Install mocha
+```
+npm install -g mocha
+```
+
+2) Run mocha
+```
+cd org/terraswarm/accessor/accessors/web/hosts/node/test/mocha
+/usr/local/bin/mocha *.js
+```
+
+See https://chess.eecs.berkeley.edu/ptexternal/wiki/Main/JSMocha
---
+++ trunk/accessors/web/hosts/node/test/mocha/testNodeHost.js
@@ -0,0 +1,21 @@
+// @version: $$Id$$
+// Run the test/TestComposite code in accessors/web/test/TestComposite.js
+// To run this test, do:
+//   sudo npm install -g mocha
+//   mocha testNodeHost.js
+
+var nodeHost = require('../../nodeHost.js');
+var assert = require('assert');
+describe('AccessorTests', function () {
+    describe('test/TestComposite()', function () {
+        it('Create a test composite and run it', function () {
+            var a = this.instantiate('TestComposite', 'test/TestComposite');
+            a.initialize();
+            a.provideInput('input', 10);
+            a.react();
+            // Should return 50
+            assert.equal(a.latestOutput('output'), 50); 
+            a.wrapup();
+        });
+    });
+});
