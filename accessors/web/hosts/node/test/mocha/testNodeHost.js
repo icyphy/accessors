713505e3e3d833812fbd72917d84793263f39c46
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-02-13 19:36:00 +0000

    Don't use this. inside Mocha tests.

--- accessors/web/hosts/node/test/mocha/testNodeHost.js
+++ accessors/web/hosts/node/test/mocha/testNodeHost.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 describe('AccessorTests', function () {
     describe('test/TestComposite()', function () {
         it('Create a test composite and run it', function () {
-            var a = this.instantiate('TestComposite', 'test/TestComposite');
+            var a = /*this.*/instantiate('TestComposite', 'test/TestComposite');
             a.initialize();
             a.provideInput('input', 10);
             a.react();
