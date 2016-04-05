d25a4597e2a9612528037d2fc382336a372ba3c4
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-04-05 23:27:00 +0000

    Added second test that delays so that the TestSpontaneous tests in test/testCommon.js can run to completion.

--- trunk/accessors/web/hosts/node/test/mocha/testCommon.js
+++ trunk/accessors/web/hosts/node/test/mocha/testCommon.js
@@ -10,4 +10,11 @@ describe('testCommon', function () {
     it('load testCommon', function () {
         var testCommon = require('../../../../hosts/common/test/testCommon.js');
     });
+
+    it('Wait 3 seconds until the Spontaneous tests complete', function(done) {
+        // See https://mochajs.org/#timeouts
+        this.timeout(4000);
+        setTimeout(function () {done(); console.log("mocha/testCommon.js done");}, 3000);
+
+    });
 });
