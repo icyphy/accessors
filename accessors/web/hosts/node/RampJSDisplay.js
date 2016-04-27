b95deca499d67b67035775fc59db7cc37b16a308
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-04-27 16:21:52 +0000

    Add the output of generating code for ptolemy/cg/kernel/generic/accessor/test/auto/RampJSDisplay.xml

---
+++ accessors/web/hosts/node/RampJSDisplay.js
@@ -0,0 +1,23 @@
+console.log("RampJSDisplay.js start");
+exports.setup = function() {
+
+    // Ports: RampJSDisplay: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+
+    // Start: TestDisplay: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TestDisplay = this.instantiate('TestDisplay', 'test/TestDisplay.js');
+
+    // Start: TestSpontaneous: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TestSpontaneous = this.instantiate('TestSpontaneous', 'test/TestSpontaneous.js');
+    TestSpontaneous.setParameter('interval', 1000.0);
+
+    // Start: JavaScriptRamp: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
+    var JavaScriptRamp = new Accessor('JavaScriptRamp', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id$$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValue = 0;\n\nexports.initialize = function() {\n    _lastValue = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValue += this.getParameter(\'step\');\n    this.send(\'output\', _lastValue);\n};', null, null, null, null);
+    JavaScriptRamp.container = this;
+    JavaScriptRamp.setParameter('init', 0.0);
+    JavaScriptRamp.setParameter('step', 1.0);
+
+    // Connections: RampJSDisplay: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+    this.connect(JavaScriptRamp, 'output', TestDisplay, 'input');
+    this.connect(TestSpontaneous, 'output', JavaScriptRamp, 'trigger');
+}
+console.log("RampJSDisplay.js end");
