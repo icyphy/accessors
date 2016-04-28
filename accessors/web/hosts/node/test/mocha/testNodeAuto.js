abf6e007168cfbcdb60c1f8c358b99134df592f6
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-04-28 01:56:40 +0000

    Added Node Host test that runs the composite accessors in accessors/web/test/auto.

---
+++ accessors/web/hosts/node/test/mocha/testNodeAuto.js
@@ -0,0 +1,27 @@
+// @version: $$Id$$
+// Run the tests in accessors/web/test/auto.
+// To run this test, do:
+//   sudo npm install -g mocha
+//   mocha testNodeAuto.js
+// or
+//   cd accessors/web; ant tests.mocha
+
+var nodeHost = require('../../nodeHost.js');
+var fs = require('fs');
+describe('nodeHost run tests in accessors/web/test/auto', function () {
+    var accessors;
+    try {
+        // If run in accessors/web/hosts/node/test/mocha/
+        accessors = fs.readdirSync('../../../../test/auto');
+    } catch (e) {
+        // If run in accessors/web/
+        accessors = fs.readdirSync('test/auto');
+    }
+
+    accessors.forEach(function(accessor) {
+        it('NodeHost run accessors/web/test/auto/' + accessor, function () {
+            var testAccessor = [ "test/auto/" + accessor ];
+            instantiateAndInitialize(testAccessor);
+        });
+    });
+});
