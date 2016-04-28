2f32c80895ad7d73ede845bb5bb0ad138a883bed
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-04-28 23:42:54 +0000

    Added duktape/eduk/nofileio.c, which includes the .h versions of the .js files and provides a nofileio_readfile() C function that can be invoked as NoFileIo.readfile()

--- accessors/web/hosts/duktape/eduk/eduk.c
+++ accessors/web/hosts/duktape/eduk/eduk.c
@@ -19,8 +19,13 @@
 
 extern void eventloop_register(duk_context *ctx);
 extern int eventloop_run(duk_context *ctx);  /* Duktape/C function, safe called */
-extern int fileio_register(duk_context *ctx);
+
+// Use NoFileIo instead of FileIo because small embedded systems
+// don't have file systems.
+//extern int fileio_register(duk_context *ctx);
+
 extern void modSearch_register(duk_context *ctx);
+extern void nofileio_register(duk_context *ctx);
 extern void poll_register(duk_context *ctx);
 extern void print_pop_error(duk_context *ctx, FILE *f);
 
@@ -150,14 +155,12 @@ int main(int argc, char *argv[]) {
 
     // Register Modules
     eventloop_register(ctx);
-    modSearch_register(ctx);
-
     // FIXME: fileio_register() should go away eventually.
-    fileio_register(ctx);
-
+    //fileio_register(ctx);
+    modSearch_register(ctx);
+    nofileio_register(ctx);
     poll_register(ctx);
 
-    
     for (i = 1; i < argc; i++) {
         char *arg = argv[i];
         if (!arg) {
