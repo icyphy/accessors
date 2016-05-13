d5aea3a730d6ebe81069db1df52eff4b9814a976
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-05-13 18:45:18 +0000

    Reverted to r669, which works with mocha.

--- trunk/accessors/web/hosts/node/test/mocha/testCommon.js
+++ trunk/accessors/web/hosts/node/test/mocha/testCommon.js
@@ -1,39 +1,20 @@
 // @version: $$Id$$
-// Instantiate the test/Test accessor and provide the common host test file
-// as input.
-// This test requires mocha and chai.  To install these:
-// sudo npm install -g mocha
-// sudo npm install -g chai
-//
-// The test can be run using the nodeHostShell.  From this directory:
-// node ../../nodeHostShell.js < ./testCommon.js
-//
-// Output:
+// Run the test/TestComposite code in accessors/web/test/TestComposite.js
+// To run this test, do:
+//   sudo npm install -g mocha
 //   mocha testCommon.js
 
-// FIXME: we should not need to require nodeHost.js here.
-// We must require nodeHost here if running mocha from the command
-// line and running mocha from within ant is to work.
-//
-// From the command line:
-//    cd accessors/web; mocha hosts/node/test/mocha/testCommon.js
-// From ant:
-//    cd accessors/web; ant tests.mocha
-//
-// Solution: use something similar to what is in accessors/web/hosts/common/commonHost.js
-// and conditionally require nodeHost.js.
-//
-if (typeof process !== 'undefined' && typeof process.version === 'string') {
-    var nodeHost = require('../../nodeHost.js');
-}
+var nodeHost = require('../../nodeHost.js');
+var assert = require('assert');
+describe('testCommon', function () {
+    it('load testCommon', function () {
+        var testCommon = require('../../../../hosts/common/test/testCommon.js');
+    });
 
-var a = instantiate('Test', 'test/Test');
-a.initialize();
-a.provideInput('testFile', '../../../browser/test/test/testRunner.js');
-a.react();
-a.wrapup();
-// try {
-//     quit;
-// } catch (error) {
-//     console.log("testCommon.js: quit failed, this is expected under Node.");
-// }
+    it('Wait 3 seconds until the Spontaneous tests complete', function(done) {
+        // See https://mochajs.org/#timeouts
+        this.timeout(4000);
+        setTimeout(function () {done(); console.log("mocha/testCommon.js done");}, 3000);
+
+    });
+});
