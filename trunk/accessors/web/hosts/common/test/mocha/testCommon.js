2ae57ba6145a9edc7a1e047a51b0c7f459eabfd2
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-05-13 18:55:54 +0000

    The describe() function should include the path to the test file

--- trunk/accessors/web/hosts/common/test/mocha/testCommon.js
+++ trunk/accessors/web/hosts/common/test/mocha/testCommon.js
@@ -5,7 +5,7 @@
 //   mocha TestCommon.js
 // To run all the tests, cd to the top level directory and run 'ant tests'.
 // See https://chess.eecs.berkeley.edu/ptexternal/wiki/Main/JSMocha
-describe('CommonTests', function () {
+describe('hosts/browser/common/test/mocha/testCommon.js or hosts/common/test/mocha/testCommon.js (symlink): CommonTests', function () {
     it('Load and run the accessor common host tests', function () {
         var testCommon = require('../testCommon.js');
     });
