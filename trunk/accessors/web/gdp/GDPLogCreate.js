bcb9c80d251401cdb316c14105144a8acf484043
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-08-11 21:20:30 +0000

    Added GDPLogCreate and updated other accessors to have a port parameter for the log name.

--- trunk/accessors/web/gdp/GDPLogCreate.js
+++ trunk/accessors/web/gdp/GDPLogCreate.js
@@ -1,4 +1,4 @@
-/* Read a log. */
+/* Create a Global Data Plane (GDP) log. */
 
 // Copyright (c) 2015-2016 The Regents of the University of California.
 // All rights reserved.
@@ -22,83 +22,65 @@
 // CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 // ENHANCEMENTS, OR MODIFICATIONS.
 
-/** Read a log.
+/** Create a Global Data Plane (GDP) log.
  *
- *  @output {string} data The data that is read from the log
- *
- *  @parameter {string} debugLevel The value of the GDP debug flag.
- *  See gdp/README-developers.md for a complete summary.  The value is
- *  typically "pattern=level", for example "gdplogd.physlog=39".  To
- *  see the patterns, use the "what" command or strings
- *  $PTII/lib/libgdp* | grep '@(#)'.  Use "*=40" to set the debug
- *  level to 40 for all components. The value of level is not usually
- *  over 127.  Values over 100 may modify the behavior.
- *
- *  @param {string} logname The GDP logname.  By convention, use 
+ *  @parameter {string} debugLevel The value of the GDP debug flag.  See
+ *  gdp/README.md for a complete summary.  The value is typically
+ *  "pattern=level", for example "gdplogd.physlog=39".  To see the
+ *  patterns, use the "what" command or strings $PTII/lib/libgdp* |
+ *  grep '@(#)'.  Use "*=40" to set the debug level to 40 for all
+ *  components. The value of level is not usually over 127.  Values
+ *  over 100 may modify the behavior.
+ *  @input {string} logname The GDP logname.  By convention, use 
  *  a reverse fully qualified name like
  *  "org.ptolemy.actor.lib.jjs.modules.gdp.demo.GDPLogRead.GDPLogRead"
+ *  @input {string} logdname The IP address or DNS name of the gdp
+ *  router.  If empty, then the swarm.gdp.routers key is searched for
+ *  in the gdp configuration file ep_adm_para, in the following order
+ *  "`.ep_adm_params:~/.ep_adm_params:/usr/local/etc/ep_adm_params:/etc/ep_adm_params`"
+ *  The values of swarm.gdp.routers are then contacted in order.
+ *  @input trigger An input that triggers firing the reading of the data
+ *  @output output An output that is written to when the creation is complete.
  *
- *  @param {string} logdname The name of the logd server.  If empty,
- *  then the hostname of the local machine is used.
- *
- *  @input trigger An input that triggers firing the reading of the data.
- *
- *  @input {int} recno The record number to be read.  In the GDP,
- *  the first record is record 1.
- *
- *  @author Edward A. Lee, Nitesh Mor. Contributor: Christopher Brooks
- *  @version $$Id$$ 
+ *  @author Christopher Brooks
+ *  @version $$Id: GDPLogCreate.js 865 2016-07-20 14:11:58Z cxh $$ 
  */
 
 // Stop extra messages from jslint.  Note that there should be no
 // space between the / and the * and global.
-/*globals addInputHandler, exports, get, getParameter, input, output, parameter, removeInputHandler, require, send */
+/*globals addInputHandler, addInputParameter, console, exports, get, getParameter, input, parameter, removeInputHandler, require */
 /*jshint globalstrict: true */
 "use strict";
 
 var GDP = require('gdp');
-var handle = null;
 var log = null;
-var oldLogname = null;
+var handle = null;
 
 /** Setup the parameters and ports. */
 exports.setup = function() {
-    this.output('data', {'type': 'string'});
     this.parameter('debugLevel', {'type': 'string'});
     this.input('logname', {'type': 'string', 'value': 'myLog'});
     this.input('logdname', {'type': 'string', 'value': ''});
+    this.output('output', {'type': 'string'});
     this.input('trigger');
-    this.input('recno', {'type': 'int'});
 };
 
-
-/** Read a record and send the data on the output.
- *  Note that if the log does not exist, it will be created
- *  and a null will be sent.
+/** If the log is not present, then create it.
  */
-exports.read = function() {
-    var recno = this.get('recno');
+exports.create = function() {
     var logname = this.getParameter('logname');
     if (logname === '') {
         throw new Error('The logname parameter cannot be empty.');
     }
-    if (logname != oldLogname) {
-	// console.log("GDPLogRead.read(): About to call new GDP.GDP()");
-	var logdname = this.getParameter('logdname');
-	log = new GDP.GDP(logname, 1, logdname);
-	log.setDebugLevel(this.getParameter('debugLevel'));
-	oldLogname = logname;
-	this.send('data', null);
-    } else {
-	var data = log.read(recno);
-	this.send('data', data);
-    }
+    var logdname = this.getParameter('logdname');
+    log = new GDP.GDP(logname, 3, logdname);
+    log.setDebugLevel(this.getParameter('debugLevel'));
+    this.send('output', logname);
 };
 
-/** Add an input handler that will read data. */
+/** Add an input handler that will create the log. */
 exports.initialize = function() {
-    oldLogname = null;
-    handle = this.addInputHandler('trigger', this.exports.read.bind(this));
+    handle = this.addInputHandler('trigger', this.exports.create.bind(this));
 };
 
 /** Remove the input handler. */
