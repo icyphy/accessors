a766c893bb7efc9b0e1949669f8dbee1ddadd3f6
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-08-12 21:37:56 +0000

    Added TestSpontanousOnce, which fires only once.

---
+++ accessors/web/test/TestSpontaneousOnce.js
@@ -0,0 +1,58 @@
+// Test accessor that spontaneously produces outputs once.
+//
+// Copyright (c) 2016 The Regents of the University of California.
+// All rights reserved.
+
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+
+/** Test accessor that spontaneously produces outputs once per time interval.
+ *  This implementation produces a counting sequence.
+ *
+ *  @accessor test/TestSpontaneousOnce
+ *  @parameter interval The interval before the output in milliseconds.
+ *  @output output Output for the counting sequence, of type number.
+ *  @author Christopher Brooks. Based on TestSpontaneous by Edward A. Lee
+ *  @version $$Id: TestSpontaneous.js 546 2016-02-03 02:07:57Z cxh $$
+ */
+
+exports.setup = function() {
+    this.parameter('interval', {'type':'number', 'value':1000});
+    this.output('output', {'type': 'number'});
+};
+
+// These variables will not be visible to subclasses.
+var handle = null;
+var count = 0;
+
+exports.initialize = function() {
+    count = 0;
+    // Need to record 'this' for use in the callback.
+    var thiz = this;
+    handle = setTimeout(function() {
+        thiz.send('output', count++);
+    }, this.getParameter('interval'));
+};
+
+exports.wrapup = function() {
+    if (handle) {
+        clearTimeoutInterval(handle);
+        handle = null;
+    }
+};
