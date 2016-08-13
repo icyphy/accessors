5e16035e67a1922c4924ec5f2585dc33d637a416
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-08-13 01:51:38 +0000

    Working on GDP Subscription.

--- accessors/web/gdp/test/auto/GDPLogSubscribeJS.js
+++ accessors/web/gdp/test/auto/GDPLogSubscribeJS.js
@@ -7,6 +7,25 @@ exports.setup = function() {
 
     // Ports: GDPLogSubscribeJS: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
+    // Start: GDPLogSubscribe: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var GDPLogSubscribe = this.instantiate('GDPLogSubscribe', 'gdp/GDPLogSubscribe.js');
+    GDPLogSubscribe.setParameter('debugLevel', "");
+    GDPLogSubscribe.setDefault('logdname', "edu.berkeley.eecs.gdp-01.gdplogd");
+    GDPLogSubscribe.setDefault('logname', "myLog");
+    GDPLogSubscribe.setParameter('numrec', 0);
+    GDPLogSubscribe.setParameter('startrec', 0);
+    GDPLogSubscribe.setParameter('timeout', 0);
+
+    // Start: TestSpontaneous3: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TestSpontaneous3 = this.instantiate('TestSpontaneous3', 'test/TestSpontaneous.js');
+    TestSpontaneous3.setParameter('interval', 1500.0);
+
+    // Start: TrainableTest2: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TrainableTest2 = this.instantiate('TrainableTest2', 'test/TrainableTest.js');
+    TrainableTest2.setParameter('correctValues', ["datum was null?","2","3"]);
+    TrainableTest2.setParameter('trainingMode', false);
+    TrainableTest2.setParameter('tolerance', 1.0E-9);
+
     // Start: GDPLogAppend: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var GDPLogAppend = this.instantiate('GDPLogAppend', 'gdp/GDPLogAppend.js');
     GDPLogAppend.setParameter('debugLevel', "");
@@ -45,7 +64,7 @@ exports.setup = function() {
     // Start: JavaScriptRamp2: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
     // FIXME: See instantiate() in accessors/web/hosts/common/commonHost.js
     // We probably need to do something with the bindings.
-    var JavaScriptRamp2 = new Accessor('JavaScriptRamp2', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id: GDPLogRead.xml 75037 2016-08-11 21:29:55Z cxh $$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValue = 0;\n\nexports.initialize = function() {\n    _lastValue = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValue += this.getParameter(\'step\');\n    this.send(\'output\', _lastValue);\n};', null, null, null, null);
+    var JavaScriptRamp2 = new Accessor('JavaScriptRamp2', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id: GDPLogRead.xml 75037 2016-08-11 21:29:55Z cxh $$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValueJavaScriptRamp2 = 0;\n\nexports.initialize = function() {\n    _lastValueJavaScriptRamp2 = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValueJavaScriptRamp2 += this.getParameter(\'step\');\n    this.send(\'output\', _lastValueJavaScriptRamp2);\n};', null, null, null, null);
     JavaScriptRamp2.container = this;
     this.containedAccessors.push(JavaScriptRamp2);
     JavaScriptRamp2.setParameter('init', 0);
@@ -53,8 +72,8 @@ exports.setup = function() {
 
     // Start: TrainableTest: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var TrainableTest = this.instantiate('TrainableTest', 'test/TrainableTest.js');
-    TrainableTest.setParameter('correctValues', [null,"2","3","4","5"]);
-    TrainableTest.setParameter('trainingMode', false);
+    TrainableTest.setParameter('correctValues', [null,"1","2","3","4","5"]);
+    TrainableTest.setParameter('trainingMode', true);
     TrainableTest.setParameter('tolerance', 1.0E-9);
 
     // Start: JavaScriptGDPLogName: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
@@ -66,9 +85,12 @@ exports.setup = function() {
 
     // Start: TestSpontaneousOnce: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var TestSpontaneousOnce = this.instantiate('TestSpontaneousOnce', 'test/TestSpontaneousOnce.js');
-    TestSpontaneousOnce.setParameter('interval', 1000.0);
+    TestSpontaneousOnce.setParameter('interval', 500.0);
 
     // Connections: GDPLogSubscribeJS: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+    this.connect(GDPLogCreate, 'output', GDPLogSubscribe, 'logname');
+    this.connect(TestSpontaneous3, 'output', GDPLogSubscribe, 'trigger');
+    this.connect(GDPLogSubscribe, 'data', TrainableTest2, 'input');
     this.connect(GDPLogCreate, 'output', GDPLogAppend, 'logname');
     this.connect(TestSpontaneous, 'output', GDPLogAppend, 'trigger');
     this.connect(JavaScriptRamp, 'output', GDPLogAppend, 'data');
