1eb668fcb10c7fff3c9543ca8589ab4fb31c7fbd
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-08-22 21:47:06 +0000

    Added a way to run composite accessor tests in just one directory using Node.

--- accessors/web/hosts/node/test/mocha/testNodeAuto.js
+++ accessors/web/hosts/node/test/mocha/testNodeAuto.js
@@ -9,65 +9,67 @@
 var nodeHost = require('../../nodeHost.js');
 var fs = require('fs');
 
-//describe('hosts.node.test.mocha.testNodeAuto', function () {
-    var autos = ["test/auto", "net/test/auto"];
-    autos.forEach(function(auto) {
-        var accessors;
-        try {
-        // If run in accessors/web/hosts/node/test/mocha/
-            accessors = fs.readdirSync('../../../../' + auto);
-        } catch (e) {
-            // If run in accessors/web/
-            accessors = fs.readdirSync(auto);
-        }
+/** Run all the .js tests in a directory using mocha.
+ *  It is expected that the .js file define composite accessors.
+ *  @param auto The directory that contains the .js files.
+ */
+exports.testNodeAuto = function(auto) {
+    console.log("testNodeAuto.js: testNodeAuto(" + auto + ")");
+    var accessors;
+    try {
+	// If run in accessors/web/hosts/node/test/mocha/
+	accessors = fs.readdirSync('../../../../' + auto);
+    } catch (e) {
+	// If run in accessors/web/
+	accessors = fs.readdirSync(auto);
+    }
 
-        describe('testNodeAuto ' + auto.replace('/','.'), function () {
-            accessors.forEach(function(accessor) {
-
-                if (accessor.length > 3 && accessor.indexOf('.') > 0 && 
-		                accessor.indexOf('~') == -1 &&
-                		accessor.substring(0,4) != '.svn' &&
-                		accessor.substring(0,4) != '.log') {
-                	// mocha-junit-reporter creates a "classname" attribute 
-                	// with the value of the test name.  Unfortunately, Jenkins
-                	// uses any classname as the top level in the display
-                	// hierarchy.  So, we insert a "describe" with the test name
-                	// It would be better not to insert a classname attribute. 
-                	// Unfortunately, there is no option to omit it.  
-                	// Alternatively, we could post-process the file, modify
-                	// mocha-junit-reporter or try a different reporter.  
-                	// Tried mocha-jenkins-reporter, but it does not seem to 
-                	// generate a results file when passed a file path.
-                	describe('NodeHost run accessors/web/' + auto + '/' + accessor + '\n', function () {
-                        //it('NodeHost run accessors/web/' + auto + '/' + accessor + '\n', function (done) {
-                		it ('NodeHost', function(done) {
-	                        var testAccessor = [ auto +'/' + accessor ];
-	                        instantiateAndInitialize(testAccessor);
-	                        
-	                        var exception = null;
-	                        process.once('uncaughtException', function(error) { 
-	                        	exception = error;
-	                        	done(error);
-	                        });
+    // describe() is a mocha function.
+    describe('testNodeAuto ' + auto.replace('/','.'), function () {
+	    accessors.forEach(function(accessor) {
+		    if (accessor.length > 3 && accessor.indexOf('.') > 0 && 
+			accessor.endsWith('.js') &&
+			accessor.indexOf('~') == -1 &&
+			accessor.substring(0,4) != '.svn' &&
+			accessor.substring(0,4) != '.log') {
+			// mocha-junit-reporter creates a "classname" attribute 
+			// with the value of the test name.  Unfortunately, Jenkins
+			// uses any classname as the top level in the display
+			// hierarchy.  So, we insert a "describe" with the test name
+			// It would be better not to insert a classname attribute. 
+			// Unfortunately, there is no option to omit it.  
+			// Alternatively, we could post-process the file, modify
+			// mocha-junit-reporter or try a different reporter.  
+			// Tried mocha-jenkins-reporter, but it does not seem to 
+			// generate a results file when passed a file path.
+			describe('NodeHost run accessors/web/' + auto + '/' + accessor + '\n', function () {
+				// it() is a mocha function.
+				it ('NodeHost', function(done) {
+					var testAccessor = [ auto +'/' + accessor ];
+					instantiateAndInitialize(testAccessor);
                         
-	                        setTimeout(function(){
-	                        	if (exception === null) {
-	                        		done();
-	                        	}
-	                        }, 1000);
-	                        
-	                        // TODO:
-	                        // What is the success criteria?  Lack of exception
-	                        // e.g. from trainable test after certain amount of 
-	                        // time?  any way to listen for a stop?
-	                        // Node-host-specific solution here
-	                        
-	                        // Cancel timer in case of a stop?
-                		});
-                	});
-                	}
-
-                });
-            });
-        });
-   // });
+					var exception = null;
+					process.once('uncaughtException', function(error) { 
+						exception = error;
+						done(error);
+					    });
+                        
+					setTimeout(function(){
+						if (exception === null) {
+						    done();
+						}
+					    }, 1000);
+                        
+					// TODO:
+					// What is the success criteria?  Lack of exception
+					// e.g. from trainable test after certain amount of 
+					// time?  any way to listen for a stop?
+					// Node-host-specific solution here
+                        
+					// Cancel timer in case of a stop?
+				    });
+			    });
+		    }
+		});
+	});
+};
