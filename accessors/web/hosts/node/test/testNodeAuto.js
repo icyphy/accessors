e5fd98d30cc1c69542ba494878a689a4915bf4fb
beth <21232697+bethosyk@users.noreply.github.com>
2016-08-25 18:51:01 +0000

    Node regression test results report now omits classname attribute and has one-level hierarchy.

--- accessors/web/hosts/node/test/testNodeAuto.js
+++ accessors/web/hosts/node/test/testNodeAuto.js
@@ -25,51 +25,51 @@ exports.testNodeAuto = function(auto) {
     }
 
     // describe() is a mocha function.
-    describe('testNodeAuto ' + auto.replace('/','.'), function () {
-	    accessors.forEach(function(accessor) {
-		    if (accessor.length > 3 && accessor.indexOf('.') > 0 && 
-			accessor.endsWith('.js') &&
-			accessor.indexOf('~') == -1 &&
-			accessor.substring(0,4) != '.svn' &&
-			accessor.substring(0,4) != '.log') {
-			// mocha-junit-reporter creates a "classname" attribute 
-			// with the value of the test name.  Unfortunately, Jenkins
-			// uses any classname as the top level in the display
-			// hierarchy.  So, we insert a "describe" with the test name
-			// It would be better not to insert a classname attribute. 
-			// Unfortunately, there is no option to omit it.  
-			// Alternatively, we could post-process the file, modify
-			// mocha-junit-reporter or try a different reporter.  
-			// Tried mocha-jenkins-reporter, but it does not seem to 
-			// generate a results file when passed a file path.
-			describe('NodeHost run accessors/web/' + auto + '/' + accessor + '\n', function () {
-				// it() is a mocha function.
-				it ('NodeHost', function(done) {
-					var testAccessor = [ auto +'/' + accessor ];
-					instantiateAndInitialize(testAccessor);
-                        
-					var exception = null;
-					process.once('uncaughtException', function(error) { 
-						exception = error;
-						done(error);
+    describe('NodeHost' , function() {
+	    //describe('testNodeAuto ' + auto.replace('/','.'), function () {
+		    accessors.forEach(function(accessor) {
+			    if (accessor.length > 3 && accessor.indexOf('.') > 0 && 
+				accessor.endsWith('.js') &&
+				accessor.indexOf('~') == -1 &&
+				accessor.substring(0,4) != '.svn' &&
+				accessor.substring(0,4) != '.log') {
+				// mocha-junit-reporter creates a "classname" attribute 
+				// with the value of the test name.  Unfortunately, Jenkins
+				// uses any classname as the top level in the display
+				// hierarchy.  So, we insert a "describe" with the test name
+				// It would be better not to insert a classname attribute. 
+				// Unfortunately, there is no option to omit it.  
+				// Alternatively, we could post-process the file, modify
+				// mocha-junit-reporter or try a different reporter.  
+				// Tried mocha-jenkins-reporter, but it does not seem to 
+				// generate a results file when passed a file path.
+					// it() is a mocha function.
+					it ('run accessors/web/' + auto + '/' + accessor + '\n', function (done) {
+						var testAccessor = [ auto +'/' + accessor ];
+						instantiateAndInitialize(testAccessor);
+	                        
+						var exception = null;
+						process.once('uncaughtException', function(error) { 
+							exception = error;
+							done(error);
+						    });
+	                        
+						setTimeout(function(){
+							if (exception === null) {
+							    done();
+							}
+						    }, 1000);
+	                        
+						// TODO:
+						// What is the success criteria?  Lack of exception
+						// e.g. from trainable test after certain amount of 
+						// time?  any way to listen for a stop?
+						// Node-host-specific solution here
+	                        
+						// Cancel timer in case of a stop?
 					    });
-                        
-					setTimeout(function(){
-						if (exception === null) {
-						    done();
-						}
-					    }, 1000);
-                        
-					// TODO:
-					// What is the success criteria?  Lack of exception
-					// e.g. from trainable test after certain amount of 
-					// time?  any way to listen for a stop?
-					// Node-host-specific solution here
-                        
-					// Cancel timer in case of a stop?
-				    });
-			    });
-		    }
-		});
+			    }
+			//});
+	    });
 	});
 };
