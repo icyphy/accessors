09c8f3a3709dc48cc2b4364b0a7fd281e8b788cf
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-09-03 05:38:32 +0000

    Regenerated tests from models.

--- accessors/web/test/auto/RampJSTestDisplay.js
+++ accessors/web/test/auto/RampJSTestDisplay.js
@@ -1,5 +1,7 @@
 exports.setup = function() {
     //  This composite accessor was created by Cape Code.
+    //  To run the code, run: 
+    //  (cd /Users/cxh/ptII/org/terraswarm/accessor/accessors/web/test/auto; node ../../hosts/node/nodeHostInvoke.js -timeout 6000 test/auto/RampJSTestDisplay)
     //  To regenerate this composite accessor, run:
     //  java -classpath $PTII ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTestDisplay.xml
     //  to edit the model, run:
@@ -12,8 +14,11 @@ exports.setup = function() {
     TestSpontaneous.setParameter('interval', 1000.0);
 
     // Start: JavaScriptRamp: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
+    // FIXME: See instantiate() in accessors/web/hosts/common/commonHost.js
+    // We probably need to do something with the bindings.
     var JavaScriptRamp = new Accessor('JavaScriptRamp', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id$$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValue = 0;\n\nexports.initialize = function() {\n    _lastValue = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValue += this.getParameter(\'step\');\n    this.send(\'output\', _lastValue);\n};', null, null, null, null);
     JavaScriptRamp.container = this;
+    this.containedAccessors.push(JavaScriptRamp);
     JavaScriptRamp.setParameter('init', 0.0);
     JavaScriptRamp.setParameter('step', 1.0);
 
--- accessors/web/test/auto/TestComposite.js
+++ accessors/web/test/auto/TestComposite.js
@@ -1,8 +1,10 @@
 exports.setup = function() {
     //  This composite accessor was created by Cape Code.
-    //  To regenerate run: 
+    //  To run the code, run: 
+    //  (cd /Users/cxh/ptII/org/terraswarm/accessor/accessors/web/test/auto; node ../../hosts/node/nodeHostInvoke.js -timeout 2000 test/auto/TestComposite)
+    //  To regenerate this composite accessor, run:
     //  java -classpath $PTII ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
-    //  or 
+    //  to edit the model, run:
     //  $PTII/bin/vergil -capecode $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
 
     // Ports: TestComposite: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
@@ -11,13 +13,13 @@ exports.setup = function() {
 
     // Start: TestGain: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var TestGain = this.instantiate('TestGain', 'test/TestGain.js');
-    // TestGain.setParameter('input', 0);
+    TestGain.setDefault('input', 0);
     TestGain.setParameter('gain', 4.0);
 
     // Start: TestAdder: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var TestAdder = this.instantiate('TestAdder', 'test/TestAdder.js');
-    // TestAdder.setParameter('inputLeft', 0);
-    // TestAdder.setParameter('inputRight', 0);
+    TestAdder.setDefault('inputLeft', 0);
+    TestAdder.setDefault('inputRight', 0);
 
     // Connections: TestComposite: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
     this.connect('input', TestGain, 'input');
