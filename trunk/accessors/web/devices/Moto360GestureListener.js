af4ab06a7fb4059a95d8456aaa4540f5868c2c4f
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-10-05 02:06:12 +0000

    Moto360GestureListener.js extends UDPSocketListener.

--- trunk/accessors/web/devices/Moto360GestureListener.js
+++ trunk/accessors/web/devices/Moto360GestureListener.js
@@ -23,15 +23,17 @@
 /** 
  * Read UDP gesture data from a Moto 360 watch.
  *  
- *  @accessor devices/Moto360GestureListener
- *  @output {string} message The received message.
- *  @output {boolean} listening True to indicate that listening has begun, false to
+ * See https://github.com/Zziwei/GestureUDP for software for the watch.
+ *
+ * @accessor devices/Moto360GestureListener
+ * @output {string} message The received message.
+ * @output {boolean} listening True to indicate that listening has begun, false to
  *   indicate that it has stopped.
  *
- *  @input {string} listeningAddress The interface to listen on for incoming messages.
+ * @input {string} listeningAddress The interface to listen on for incoming messages.
  *   This defaults to "0.0.0.0", which means to listen on all network interfaces.
- *  @input {int} listeningPort The port to listen on for incoming messages.
- *   This defaults to 8084.
+ * @input {int} listeningPort The port to listen on for incoming messages.
+ *   This defaults to 4567, which is the value found in https://github.com/Zziwei/PackageSendTest
  *   
  *  @parameter {string} receiveType See above.
 
@@ -45,9 +47,44 @@
 /*jshint globalstrict: true*/
 "use strict";
 
+// This accessor requires the optional 'udpSocket' module, which may or may
+// not be provided by an accessor host. Most hosts will provide this module.
+var UDPSocket = require('udpSocket');
+
 /** Set up the accessor by defining the inputs and outputs.
  */
 exports.setup = function () {
     this.extend('net/UDPSocketListener');
+    this.input('listeningPort', {
+        'value': 4567, // 4567 is the value found in https://github.com/Zziwei/PackageSendTest
+        'type': 'int'
+    });
+    this.parameter('receiveType', {
+        type: 'string',
+        value: 'unsignedbyte',
+    });
+};
+
+exports.initialize = function () {
+    exports.ssuper.initialize.call(this);
+    console.log("Moto360GesterListener.js: initialize()");
+};
+
+exports.closeAndOpen = function () {
+    console.log("Moto360GestureListener.js: closeAndOpen()");
+
+    exports.ssuper.closeAndOpen.call(this);
+    var self = this;
+
+    exports.ssuper.socket.on('message', function (message) {
+        console.log("Moto360GestureListener: sending message0");
+        if (exports.ssuper.running) {
+            console.log("Moto360GestureListener: sending message");
+            self.send('message', message);
+            // Here's where Moto360GestureListener differs from UDPSocketListener.
+
+            // Here, we want to parse the data
+        }
+    });
 };
 
