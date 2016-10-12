e72693a9390ca4379e45822d0ded2e4060e6adec
beth <21232697+bethosyk@users.noreply.github.com>
2016-10-12 17:40:17 +0000

    Basic Hue test for watch-to-hue demo.

---
+++ accessors/web/devices/test/TestHue.js
@@ -0,0 +1,42 @@
+exports.setup = function() {
+    //  This composite accessor was created by Cape Code.
+    //  To run the code, run: 
+    //  (cd C:\workspaceluna\ptII\org\terraswarm\accessor\accessors\web\net\test\auto; node ../../../hosts/node/nodeHostInvoke.js -timeout 16000 net/test/auto/TestHue)
+    //  To regenerate this composite accessor, run:
+    //  java -classpath $PTII ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor file:/C:/workspaceluna/ptII/org/terraswarm/accessor/demo/WatchHue/TestHue.xml
+    //  to edit the model, run:
+    //  $PTII/bin/vergil -capecode file:/C:/workspaceluna/ptII/org/terraswarm/accessor/demo/WatchHue/TestHue.xml
+
+    // Ports: TestHue: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+
+    // Start: TestSpontaneousOnce: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TestSpontaneousOnce = this.instantiate('TestSpontaneousOnce', 'test/TestSpontaneousOnce.js');
+    TestSpontaneousOnce.setParameter('interval', 1000.0);
+
+    // Start: Command: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
+    // FIXME: See instantiate() in accessors/web/hosts/common/commonHost.js
+    // We probably need to do something with the bindings.
+    var Command = new Accessor('Command', '/** Generate a string.\n *\n *  @accessor String\n *  @input trigger When an input is received, output the string.\n *  @output output The string.\n *  @author Elizabeth Osyk\n *  @version $$Id$$\n */\nexports.setup = function() {\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\' : \'JSON\'});\n};\n\nexports.initialize = function() {\n    var self = this;\n    this.addInputHandler(\'trigger\', function() {\n    	// On, off, on, off\n       var data = [{\'id\':1, \'on\':true}, {\'id\':2, \'on\':true}];\n	   self.send(\'output\', data);\n	   \n	   setTimeout(function() {\n	   	data = [{\'id\':1, \'on\':false}, {\'id\':2, \'on\':false}];\n	    self.send(\'output\', data);\n	    \n	    setTimeout(function() {\n		   	data = [{\'id\':1, \'on\':true}, {\'id\':2, \'on\':true}];\n		    self.send(\'output\', data);\n		    \n		    setTimeout(function() {\n	   			data = [{\'id\':1, \'on\':false}, {\'id\':2, \'on\':false}];\n	    		self.send(\'output\', data);\n		   		}, 3000);\n	    }, 3000);\n	   }, 3000);\n	   \n\n	   \n    });\n}\n', null, null, null, null);
+    Command.container = this;
+    this.containedAccessors.push(Command);
+
+    // Start: Delay: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
+    // FIXME: See instantiate() in accessors/web/hosts/common/commonHost.js
+    // We probably need to do something with the bindings.
+    var Delay = new Accessor('Delay', '/** Generate a string.\n *\n *  @accessor String\n *  @input trigger When an input is received, output the string.\n *  @output output The string.\n *  @author Elizabeth Osyk\n *  @version $$Id$$\n */\nexports.setup = function() {\n    this.input(\'input\');\n    this.output(\'output\');\n    this.parameter(\'delay\');\n};\n\nexports.initialize = function() {\n    var self = this;\n    this.addInputHandler(\'input\', function() {\n    	var delay = self.get(\'delay\');\n    	setTimeout(function() {\n    	       self.send(\'output\', self.get(\'input\'));\n    	}, delay);\n\n    });\n}\n', null, null, null, null);
+    Delay.container = this;
+    this.containedAccessors.push(Delay);
+    Delay.setParameter('delay', 3000);
+
+    // Start: Hue: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var Hue = this.instantiate('Hue', 'devices/Hue.js');
+    Hue.setDefault('commands', {});
+    Hue.setParameter('bridgeIP', "192.168.254.15");
+    Hue.setParameter('userName', "S8y15EK-24ug2W3R56FxmrdchCs6-A86ubAIDDpn");
+    Hue.setParameter('onWrapup', "none");
+
+    // Connections: TestHue: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+    this.connect(Delay, 'output', Command, 'trigger');
+    this.connect(TestSpontaneousOnce, 'output', Delay, 'input');
+    this.connect(Command, 'output', Hue, 'commands');
+}
