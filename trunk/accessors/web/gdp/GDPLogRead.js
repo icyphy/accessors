1d4ed3732fa1efdbcf62c95319d23d238d4b39e2
eal <eal@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-10-22 19:09:50 +0000

    Partially working GDP subscribe

--- trunk/accessors/web/gdp/GDPLogRead.js
+++ trunk/accessors/web/gdp/GDPLogRead.js
@@ -22,21 +22,13 @@
 // CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 // ENHANCEMENTS, OR MODIFICATIONS.
 
-/** Read a log.
+/** Read a log from the Global Data Plane (GDP).
  *
  *  @output {string} data The data that is read from the log
  *
- *  @parameter {string} debugLevel The value of the GDP debug flag.
- *  See gdp/README-developers.md for a complete summary.  The value is
- *  typically "pattern=level", for example "gdplogd.physlog=39".  To
- *  see the patterns, use the "what" command or strings
- *  $PTII/lib/libgdp* | grep '@(#)'.  Use "*=40" to set the debug
- *  level to 40 for all components. The value of level is not usually
- *  over 127.  Values over 100 may modify the behavior.
- *
  *  @param {string} logname The GDP logname.  By convention, use 
  *  a reverse fully qualified name like
- *  "org.ptolemy.actor.lib.jjs.modules.gdp.demo.GDPLogRead.GDPLogRead"
+ *  "org.terraswarm.accessors.demo.MyDemoName"
  *
  *  @param {string} logdname The name of the logd server.  If empty,
  *  then the hostname of the local machine is used.
@@ -46,6 +38,15 @@
  *  @input {int} recno The record number to be read.  In the GDP,
  *  the first record is record 1.
  *
+ *  @parameter {string} debugLevel The value of the GDP debug flag.
+ *  See gdp/README-developers.md for a complete summary.  The value is
+ *  typically "pattern=level", for example "gdplogd.physlog=39".  To
+ *  see the patterns, use the "what" command or strings
+ *  $PTII/lib/libgdp* | grep '@(#)'.  Use "*=40" to set the debug
+ *  level to 40 for all components. The value of level is not usually
+ *  over 127.  Values over 100 may modify the behavior.
+ *
+ *  @accessor gdp/GDPLogRead
  *  @author Edward A. Lee, Nitesh Mor. Contributor: Christopher Brooks
  *  @version $$Id$$ 
  */
@@ -65,12 +66,11 @@ var oldLogname = null;
 exports.setup = function() {
     this.output('data', {'type': 'string'});
     this.parameter('debugLevel', {'type': 'string'});
-    this.input('logname', {'type': 'string', 'value': 'myLog'});
-    this.input('logdname', {'type': 'string', 'value': ''});
+    this.input('logname', {'type': 'string', 'value': 'org.terraswarm.accessors.demo.MyDemoName'});
+    this.input('logdname', {'type': 'string', 'value': 'edu.berkeley.eecs.gdp-01.gdplogd'});
     this.input('trigger');
     this.input('recno', {'type': 'int'});
 };
-
 
 /** Read a record and send the data on the output.
  *  Note that if the log does not exist, it will be created
@@ -84,22 +84,21 @@ exports.read = function() {
         throw new Error('The logname parameter cannot be empty.');
     }
     if (logname != oldLogname) {
-	console.log("GDPLogRead.read(" + recno + "): About to call new GDP.GDP()");
-	var logdname = this.get('logdname');
-	log = new GDP.GDP(logname, 1, logdname);
-	log.setDebugLevel(this.getParameter('debugLevel'));
-	oldLogname = logname;
+	    console.log("GDPLogRead.read(" + recno + "): About to call new GDP.GDP()");
+	    var logdname = this.get('logdname');
+	    log = new GDP.GDP(logname, 1, logdname);
+	    log.setDebugLevel(this.getParameter('debugLevel'));
+	    oldLogname = logname;
     }
     // FIXME: If recno == 0, then calling new GDP.GDP() and then trying to read results in 'ERROR: 404 not found [Berkeley:Swarm-GDP:404]'
-    if (recno == 0) {
-	console.log("GDPLogRead.read(" + recno + "): recno was 0, sending nil");
-	this.send('data', 'nil');
+    if (recno <= 0) {
+	    console.log("GDPLogRead.read(" + recno + "): recno was 0, sending nil");
+	    this.send('data', 'nil');
     } else {
-	var data = log.read(recno);
-	console.log("GDPLogRead.read(" + recno + "): sending " + data);
-	this.send('data', data);
+	    var data = log.read(recno);
+	    console.log("GDPLogRead.read(" + recno + "): sending " + data);
+	    this.send('data', data);
     }
-
 };
 
 /** Add an input handler that will read data. */
