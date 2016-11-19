34bfab298ddd870df97780d6d4982ea251212765
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-11-19 01:45:12 +0000

    Nashorn: Added start on test for web/test/auto/*.js composite accessors.

---
+++ trunk/accessors/web/hosts/nashorn/test/testNashornAuto.js
@@ -0,0 +1,27 @@
+// Run the tests in accessors/web/test/auto/
+//
+// To run this:
+//   cd accessors/web/hosts
+//   ./nashorn/nashornAccessorHost -js -timeout 5000 nashorn/test/testNashornAuto.js 
+//
+// Or, use ant!
+//   cd accessors/web
+//   ant tests.nashorn
+
+// Generate a list of auto files in ../test/auto
+var File = Java.type('java.io.File');
+var directory = new File('../test/auto');
+var directoryFiles = directory.listFiles();
+var jsFiles = [];
+var i;
+
+for(i = 0; i < directoryFiles.length; i += 1) {
+    var fileName = directoryFiles[i].getPath();
+    if (fileName.endsWith('.js')) {
+	jsFiles.push(fileName);
+    }
+}
+
+instantiateAndInitialize(jsFiles);
+
+console.log("nashorn/test/testNashornAuto.js: end");
