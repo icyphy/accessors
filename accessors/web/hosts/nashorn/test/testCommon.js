bbfae1e5d47b41ce79cb724df6555224385d8d41
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-11-19 01:45:12 +0000

    Nashorn: Added start on test for web/test/auto/*.js composite accessors.

---
+++ accessors/web/hosts/nashorn/test/testCommon.js
@@ -0,0 +1,13 @@
+// Run the common tests.
+//
+// To run this:
+//   cd accessors/web/hosts
+//   ./nashorn/nashornAccessorHost -js -timeout 5000 nashorn/test/testCommon.js 
+//
+// Or, use ant!
+//   cd accessors/web
+//   ant tests.nashorn
+
+var testCommon = require("common/test/testCommon");
+
+console.log("nashorn/test/testCommon.js: end");
