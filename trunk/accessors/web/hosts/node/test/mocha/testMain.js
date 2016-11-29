0fe3f90a9cc1f050f92bff3deb703e1502f834e9
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-11-29 01:40:00 +0000

    commonHost.js main(): Deal with cases where -timeout is the last argument.

--- trunk/accessors/web/hosts/node/test/mocha/testMain.js
+++ trunk/accessors/web/hosts/node/test/mocha/testMain.js
@@ -7,14 +7,19 @@
 var nodeHost = require('../../nodeHost.js');
 var assert = require('assert');
 describe('hosts/node/test/mocha/testMain.js: commonHost.js main()', function () {
-    it('main(-h)', function () {
+    it('main()', function () {
 	var args = [];
+	// 3 is the error code
 	assert.equal(main(args), 3);
     });
     it('main(-h)', function () {
 	var args = [ '-h' ];
 	// FIXME: It would be nice to catch the output here
 	assert.equal(main(args), 0);
+    });
+    it('main(-timeout) with no timeout', function () {
+	var args = ['-timeout'];
+	assert.equal(main(args), 3);
     });
     it('main(-v)', function () {
 	var args = [ '-v' ];
