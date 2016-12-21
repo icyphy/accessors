7d1657c2b8cd072a3833be729ef355538fe9b80a
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-12-21 01:03:52 +0000

    Updated comment in generated composite accessors to reflect that nodeHostInvoke.js now takes --accessor.

--- accessors/web/test/auto/RampJSTest.js
+++ accessors/web/test/auto/RampJSTest.js
@@ -1,7 +1,7 @@
 exports.setup = function() {
     //  This composite accessor was created by Cape Code.
     //  To run the code, run: 
-    //  (cd $PTII/org/terraswarm/accessor/accessors/web/test/auto; node ../../hosts/node/nodeHostInvoke.js -timeout 6000 test/auto/RampJSTest)
+    //  (cd $PTII/org/terraswarm/accessor/accessors/web/test/auto; node ../../hosts/node/nodeHostInvoke.js --accessor -timeout 6000 test/auto/RampJSTest)
     //  To regenerate this composite accessor, run:
     //  java -classpath $PTII ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTest.xml
     //  to edit the model, run:
