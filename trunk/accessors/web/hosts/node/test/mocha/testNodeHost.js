e860a2dbb510fa0fbe22ad5cbc552a2ded4073bd
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2016-12-27 15:27:32 +0000

    Use nodeHost.instantiate() because we use require nodeHost.js earlier.

--- trunk/accessors/web/hosts/node/test/mocha/testNodeHost.js
+++ trunk/accessors/web/hosts/node/test/mocha/testNodeHost.js
@@ -9,7 +9,7 @@ var assert = require('assert');
 describe('hosts/node/test/mocha/testNodeHost.js: testComposite', function () {
     describe('test/TestComposite()', function () {
         it('Create a test composite and run it', function () {
-            var a = /*this.*/instantiate('TestComposite', 'test/TestComposite');
+            var a = /*this.*/nodeHost.instantiate('TestComposite', 'test/TestComposite');
             a.initialize();
             a.provideInput('input', 10);
             a.react();
