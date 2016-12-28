08e784f90197354228c21d79fad0621c88ac27e0
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
2016-12-28 00:04:04 +0000

    Fix undefined bindings variable and adjusted to work with Nashorn

--- accessors/web/hosts/nashorn/test/testNashornHost.js
+++ accessors/web/hosts/nashorn/test/testNashornHost.js
@@ -23,13 +23,13 @@
 // This is just a simple smoke test for the Nashorn Accessor Host.
 // To run it:
 //    cd ..
-//    ./nashorn/nashornAccessorHost -timeout 10000 hosts/nashorn/test/testNashornHost.js
+//    ./nashornAccessorHost -timeout 10000 hosts/nashorn/test/testNashornHost.js
 //
 // Or, use ant!
 //    cd ../..
 //    ant tests.nashorn
 //
-var a = this.instantiate('TestComposite', 'test/TestComposite');
+var a = instantiate('TestComposite', 'test/TestComposite');
 a.initialize();
 a.provideInput('input', 10);
 a.react();
