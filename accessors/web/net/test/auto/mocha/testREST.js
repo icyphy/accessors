04970ccec835609aa9c838c87f92c949c8c4917f
Christopher Brooks <cxh@eecs.berkeley.edu>
2016-12-28 17:56:29 +0000

    describe() now includes how to replicate.  Reindented.

--- accessors/web/net/test/auto/mocha/testREST.js
+++ accessors/web/net/test/auto/mocha/testREST.js
@@ -1,7 +1,9 @@
 // A simple Mocha test to test the regression test framework.
 // See also /accessors/hosts/common/test/testCommon.js 
 
-// This file requires mocha and chai.  MochaTest handles the mocha require.
+// This file requires mocha and chai.
+// To install mocha and chai, run "ant tests.mocha" in ../../../..
+// MochaTest handles the mocha require.
 // Note that chai's expect() does not work in strict mode; assert and should do.
 var code, instance;
 var host = require('../../../../hosts/node/nodeHost.js');
@@ -9,92 +11,85 @@ var chai = require('chai');
 var assert = chai.assert;        
 var should = chai.should();
 
-describe('net/REST.js', function () {
-            // Increase default timeout (originally 2000ms).
-            this.timeout(10000);
-        
-            before(function() {
+describe('net/test/auto/mocha/testREST.js.\n    To replicate: (cd net/test/auto/mocha; ../../../../node_modules/.bin/mocha testREST.js)', function () {
+    // Increase default timeout (originally 2000ms).
+    this.timeout(10000);
 
-                        // Read the accessor source code.
-                        try {
-                            console.log('instantiate ' + typeof instantiate);
-                            instance = host.instantiate('REST', 'net/REST');
-                        } catch(err){
-                            console.log(err);
-                        }
-                
-                        // Invoke the initialize function.
-                        instance.initialize();
-                
-                    });
+    before(function() {
+        // Read the accessor source code.
+        instance = host.instantiate('REST', 'net/REST');
+        // Invoke the initialize function.
+        instance.initialize();
         
-            it('Should GET values from a Cross-Origin Resource Sharing (CORS) site', function(done){
-                
-                        instance.provideInput('options', 
-                                "{\"method\" : \"GET\", \"url\" : \"https://cors-test.appspot.com/test\"}");
-                        instance.provideInput('command', "");
-                        instance.provideInput('body', "");
-                        instance.provideInput('trigger', true);
-                        instance.react(); 
-                
-                        // Wait a bit for request to complete.
-                        // FIXME:  Possible to add listener to send()?  Or callback to react()?                                
-                        setTimeout(function() {
-                                    var correctOutput = {};
-                                    correctOutput.status = "ok";
-                                    // The strict equals should.equal fails for some reason due to the 
-                                    // single quotes around 'ok', but deep.equal works.
-
-                                    // We were getting:
-                                    // TypeError: Cannot read property 'should' of undefined
-                                    //     at Timeout._onTimeout (/home/jenkins/workspace/accessors/web/net/test/auto/mocha/testREST.js:73:53)
-                                    // so we check to see if response is undefined.
-                                    var status = instance.latestOutput('status');
-                                    try {
-                                        assert.ok(typeof status !== 'undefined');
-					if (typeof status !== 'undefined') {
-                                            status.should.deep.equal(correctOutput);
-					}
-                                        done();
-                                    } catch (err) {
-                                        done(err);
-                                    }
-                                }, 3000);
-                    });
+    });
+    
+    it('Should GET values from a Cross-Origin Resource Sharing (CORS) site', function(done){
         
-            it('Should GET values using the JSON with padding technique', function(done){
+        instance.provideInput('options', 
+                              "{\"method\" : \"GET\", \"url\" : \"https://cors-test.appspot.com/test\"}");
+        instance.provideInput('command', "");
+        instance.provideInput('body', "");
+        instance.provideInput('trigger', true);
+        instance.react(); 
         
-                        instance.provideInput('options', 
-                                "{\"method\" : \"GET\", \"url\" : \"http://jsonplaceholder.typicode.com/posts/1?callback=?\"}");
-                        instance.provideInput('command', "");
-                        instance.provideInput('body', "");
-                        instance.provideInput('trigger', true);
-                        instance.react(); 
-                
-                        // Wait a bit for request to complete.
-                        // FIXME:  Possible to add listener to send()?  Or callback to react()?                                
-                        setTimeout(function() {
-                                    var correctOutput = {};
-                                    correctOutput.userId = 1;
-                                    correctOutput.id = 1;
-                                    correctOutput.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
-                                    correctOutput.body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto";
+        // Wait a bit for request to complete.
+        // FIXME:  Possible to add listener to send()?  Or callback to react()?                                
+        setTimeout(function() {
+            var correctOutput = {};
+            correctOutput.status = "ok";
+            // The strict equals should.equal fails for some reason due to the 
+            // single quotes around 'ok', but deep.equal works.
 
-                                    // We were getting:
-                                    // TypeError: Cannot read property 'should' of undefined
-                                    //     at Timeout._onTimeout (/home/jenkins/workspace/accessors/web/net/test/auto/mocha/testREST.js:73:53)
-                                    // so we check to see if response is undefined.
-                                    var response = instance.latestOutput('response');
-                                    try {
-                                        assert.ok(typeof response !== 'undefined');
-					if (typeof response !== 'undefined') {
-                                            response.should.deep.equal(correctOutput);
-					}
-                                        done();
-                                    } catch (err) {
-                                        done(err);
-                                    }
+            // We were getting:
+            // TypeError: Cannot read property 'should' of undefined
+            //     at Timeout._onTimeout (/home/jenkins/workspace/accessors/web/net/test/auto/mocha/testREST.js:73:53)
+            // so we check to see if response is undefined.
+            var status = instance.latestOutput('status');
+            try {
+                assert.ok(typeof status !== 'undefined');
+		if (typeof status !== 'undefined') {
+                    status.should.deep.equal(correctOutput);
+		}
+                done();
+            } catch (err) {
+                done(err);
+            }
+        }, 3000);
+    });
+    
+    it('Should GET values using the JSON with padding technique', function(done){
+        
+        instance.provideInput('options', 
+                              "{\"method\" : \"GET\", \"url\" : \"http://jsonplaceholder.typicode.com/posts/1?callback=?\"}");
+        instance.provideInput('command', "");
+        instance.provideInput('body', "");
+        instance.provideInput('trigger', true);
+        instance.react(); 
+        
+        // Wait a bit for request to complete.
+        // FIXME:  Possible to add listener to send()?  Or callback to react()?                                
+        setTimeout(function() {
+            var correctOutput = {};
+            correctOutput.userId = 1;
+            correctOutput.id = 1;
+            correctOutput.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
+            correctOutput.body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto";
 
-                                }, 1000);
-                    });
-        });
+            // We were getting:
+            // TypeError: Cannot read property 'should' of undefined
+            //     at Timeout._onTimeout (/home/jenkins/workspace/accessors/web/net/test/auto/mocha/testREST.js:73:53)
+            // so we check to see if response is undefined.
+            var response = instance.latestOutput('response');
+            try {
+                assert.ok(typeof response !== 'undefined');
+		if (typeof response !== 'undefined') {
+                    response.should.deep.equal(correctOutput);
+		}
+                done();
+            } catch (err) {
+                done(err);
+            }
+
+        }, 1000);
+    });
+});
