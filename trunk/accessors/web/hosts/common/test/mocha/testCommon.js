902520b21bb3cb9d4cf900ebfbd9ec1dec2a9d34
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-01-02 18:13:53 +0000

    Temporarily skip running the testCommon.js tests.

--- trunk/accessors/web/hosts/common/test/mocha/testCommon.js
+++ trunk/accessors/web/hosts/common/test/mocha/testCommon.js
@@ -5,8 +5,10 @@
 //   mocha TestCommon.js
 // To run all the tests, cd to the top level directory and run 'ant tests'.
 // See https://chess.eecs.berkeley.edu/ptexternal/wiki/Main/JSMocha
+var assert = require('assert');
 describe('hosts/browser/common/test/mocha/testCommon.js or hosts/common/test/mocha/testCommon.js (symlink): CommonTests', function () {
     it('Load and run the accessor common host tests', function () {
-        var testCommon = require('../testCommon.js');
+        // var testCommon = require('../testCommon.js');
+	assert.equal(true, false, "Temporarily failing while we figure out commonHost.");
     });
 });
