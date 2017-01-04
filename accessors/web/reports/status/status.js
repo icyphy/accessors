ceb9e9cbb7b047f08f988e5e62dda71ede824834
beth <21232697+bethosyk@users.noreply.github.com>
2017-01-04 05:26:13 +0000

    One status page to rule them all!

--- accessors/web/reports/status/status.js
+++ accessors/web/reports/status/status.js
@@ -1,11 +1,12 @@
-// An object to store the results, as results['testName'].host = status
-// e.g. results['net/REST'].node = passed;
-var results;
+// An object to store the test results, as testResults['testName'].host = status
+// e.g. testResults['net/REST'].node = passed;
+var testResults;
+var accessorMap;
 
 /** After the page has loaded, import test results and create a table.
  */
 $(window).on("load", function() {
-	results = {};
+	testResults = {};
 	
 	var url = "browserhost.txt";
 	
@@ -37,10 +38,21 @@ function parseBrowserResults(data) {
 function parseNodeResults(data) {
 	parseResults(data, 'node');
 	
-	fillTable();
+	$.ajax({
+        url: "accessorMap.txt",
+        type: "GET",
+        success: function(data) {
+        	accessorMap = JSON.parse(data);
+        	fillTable();
+        }
+     });
 };
 
-
+/** Parse test results web page.
+ * 
+ * @param data The web page.
+ * @param host The accessor host (e.g. 'node').
+ */
 function parseResults(data, host) {
 
 	// Retrieve table rows.  Pop the first (header) row.
@@ -51,80 +63,272 @@ function parseResults(data, host) {
 		// Test name, duration, result (passed / failed).
 		// TODO:  Check that this works for failed tests - additional message printed?
 		var entries = $(this).find('td');  
-		var testname = $(entries[0]).text();
+		var longtestname = $(entries[0]).text();
 		
 		// Find the accessor name, e.g., net/REST
 		// For browser tests, the accessor name is the first work of the line.
-		var accessorname;
+		var testname;
 		
 		// Skip hosts/ tests; these do not relate to a specific accessor.
-		if (testname.indexOf('hosts/') === -1) {
+		if (longtestname.indexOf('hosts/') === -1) {
 			if (host === 'node') {
 				// Some node tests begin with NodeHost run accessors/web/.
 				// Remove this.
-				if (testname.indexOf('NodeHost') >= 0) {
-					var dot = testname.indexOf('.');
+				if (longtestname.indexOf('NodeHost') >= 0) {
+					var dot = longtestname.indexOf('.');
 					if (dot > 0) {
-						testname = testname.substring(dot + 1);
+						longtestname = longtestname.substring(dot + 1);
 					}
 				}
 			}
 			
-			var space = testname.indexOf(' ');
+			var space = longtestname.indexOf(' ');
 			
 			if (space > 0) {
-				accessorname = testname.substring(0, space);
+				testname = longtestname.substring(0, space);
 			} else {
-				accessorname = testname;
+				testname = longtestname;
 			}
 			
 			// Remove any leading slashes (node host has these, browser does not).
-			if (accessorname[0] === '/') {
-				accessorname = accessorname.substring(1);
+			if (testname[0] === '/') {
+				testname = testname.substring(1);
 			}
 			
-			if (results[accessorname] === null || 
-					typeof results[accessorname] === 'undefined') {
-				results[accessorname] = {};
+			if (testResults[testname] === null || 
+					typeof testResults[testname] === 'undefined') {
+				testResults[testname] = {};
 			} 
-			results[accessorname][host] = $(entries[2]).text();
+			testResults[testname][host] = $(entries[2]).text();
 		}
 	});
 };
 
-/** Create a table from the results object.
+/** Create a table from the testResults object.
  */
 function fillTable(){
-	for (var accessorname in results) {
-		// TODO:  Alphabetize these.
+	
+	// List accessors in alphabetical order.
+	var accessornames = Object.keys(accessorMap.accessorsToHosts).sort();
+	var accessorname;
+	
+	// Create accessors-to-tests from tests-to-accessors.
+	var accessorsToTests = {};
+	var accessors;
+	var ext, row, cell0, cell1, cell2, testResult, supported, hosts, status;
+	
+	accessornames.forEach(function(name) {
+		ext = name.indexOf('.js');
+		if (ext > 0) {
+			name = name.substring(0, ext);
+		}
+		accessorsToTests[name] = [];
 		
-		// Columns are browser, node.
-		// TODO:  Add Cape Code.
-		var row = table.insertRow(1);
-		row.insertCell(0).innerHTML = accessorname;
-		var cell1 = row.insertCell(1);
-		var cell2 = row.insertCell(2);
-		
-		if (results[accessorname].browser !== null 
-				&& typeof results[accessorname].browser !== 'undefined') {
-			if (results[accessorname].browser === 'Passed') {
-				cell1.className += 'passed';
-				cell1.innerHTML = '&#9899';
-			} else {
-				cell1.className += 'failed';
-				cell1.innerHTML = 'x';
+		for (var test in accessorMap.testsToAccessors) {
+			accessors = accessorMap.testsToAccessors[test];
+			
+			if (accessors !== null && typeof accessors !== 'undefined' &&
+					accessors.length > 0) {
+
+				if(accessors.includes(name)) {
+					accessorsToTests[name].push(test);
+				}
 			}
 		}
+	});
+	
+	for (var i = accessornames.length - 1; i >= 0; i--) {
 		
-		if (results[accessorname].node !== null 
-				&& typeof results[accessorname].node !== 'undefined') {
-			if (results[accessorname].node === 'Passed') {
-				cell2.className += 'passed';
-				cell2.innerHTML = '&#9899';
-			} else {
-				cell2.className += 'failed';
-				cell2.innerHTML = 'x';
-			}
-		}	
+		accessorname = accessornames[i];
+		var shortname = accessorname;
+		
+		ext = accessorname.indexOf('.js');
+		if (ext > 0) {
+			shortname = accessorname.substring(0, ext);
+		}
+		
+		// Insert tests first.  Since rows are inserted at top of table, tests
+		// will move down as new rows are added.
+		
+		var tests = accessorsToTests[shortname];
+		var cells = {};
+
+		allTests = {};
+		allTests.browser = '';
+		allTests.node = '';
+		
+		if (tests !== null && typeof tests !== 'undefined' && tests.length > 0){
+			tests.forEach(function(test) {
+				row = table.insertRow(1);
+				row.className += 'hidden';
+				row.className += ' ' + accessorname;	// To expand/collapse tests.
+				
+				cell0 = row.insertCell(0);
+				cell0.innerHTML = test;
+				cell0.className += 'testname ';	// For left-justifying in table.
+				
+				// TODO:  Add more hosts.
+				// Does host support ALL accessors in this test?  
+				accessors = accessorMap.testsToAccessors[test];
+				
+				supported = {};
+				supported.browser = true;
+				supported.node = true;
+				
+				// Last first.
+				cells.node = row.insertCell(1);
+				cells.browser = row.insertCell(1);
+				
+				// Print each test result.
+				testResult = testResults[test];
+				
+				// Check if the host supports ALL accessors in the test case.
+				// If not, the test case is not supported.
+				accessors.forEach(function(accessor){
+					hosts = accessorMap.accessorsToHosts[accessor + '.js'];
+					if (!hosts.includes('all')) {
+						if (!hosts.includes('browser')) {
+							supported.browser = false;
+						} 
+						if (!hosts.includes('node')) {
+							supported.node = false;
+						}
+					}
+				});
+				
+				// Fill in results of this test case.
+				fillCells(cells, supported, testResult);
+
+				// Did all tests for this accessor pass?
+				if (testResult !== null && typeof testResult !== 'undefined') {
+					Object.keys(testResult).forEach(function(host) {
+						if (testResult[host] !== null && 
+								typeof testResult[host] !== 'undefined') {
+							
+							if (testResult[host] === 'Passed') {
+								if (allTests[host] === '') {
+									allTests[host] = 'passed';
+								} else if (allTests[host] === 'failed') {
+									allTests[host] = 'partial';
+								}
+							} else {
+								// Everything other than 'passed' considered a 
+								// failure.
+								if (allTests[host] === '') {
+									allTests[host] = 'failed';
+								} else if (allTests[host] === 'passed') {
+										allTests[host] = 'partial';
+								}
+							}
+						}
+						
+					});
+				}
+			});
+		}
+		
+		// Columns are browser, node.
+		row = table.insertRow(1);
+		row.className += 'collapsed';
+		row.id = accessorname;
+		cell0 = row.insertCell(0);
+		cell0.innerHTML = "+ " + accessorname;
+		cell0.className += 'name';
+		
+		cells = {};
+		
+		// Last first.
+		cells.node = row.insertCell(1);
+		cells.browser = row.insertCell(1);
+		
+		fillSummary(accessorname, cells, allTests);
+	}
+	
+	addRowHandlers();
+	
+	/** Fill out entries in a test case row, depending on results.
+	 * 
+	 * @param cells The cells to enter data into.
+	 * @param supported Fields for each host indicating if the host supports 
+	 *  all modules required by all accessors in the test case.
+	 * @param testResult Fields for each host indicating test result.
+	 */
+	function fillCells(cells, supported, testResult) {
+		Object.keys(supported).forEach(function(host) {
+			if (supported[host] === true) {
+				if(testResult !== null && typeof testResult !== 'undefined' && 
+						testResult[host] !== null && 
+						typeof testResult[host] !== 'undefined') {
+					
+					if (testResult[host] === 'Passed') {
+						cells[host].className += 'passed ';
+						cells[host].innerHTML = '&#9899';
+					} else {
+						cells[host].className += 'failed ';
+						cells[host].innerHTML = 'x';
+					}
+				}
+			}	
+			// Leave blank if not supported, even if test case fails.
+			// In some cases tests may be run on a host that doesn't support them.
+		});
+	}
+	
+	/** Fill out entries in the accessor summary row, depending on the results 
+	 * of all test cases.
+	 */
+	function fillSummary(accessor, cells, allTests) {
+		Object.keys(allTests).forEach(function(host) { 
+			// If no tests, check if host supports all modules required by the 
+			// accessor.
+			if (allTests[host] === ''){
+				if(accessorMap.accessorsToHosts[accessor].includes(host) || 
+					accessorMap.accessorsToHosts[accessor].includes('all')) {
+					cells[host].className += 'supported ';
+					cells[host].innerHTML = 'o';
+				}
+			} else if (allTests[host] === 'passed') {
+				cells[host].className += 'passed ';
+				cells[host].innerHTML = '&#9899';
+			} else if (allTests[host] === 'partial') {
+				cells[host].className += 'partial ';
+				cells[host].innerHTML = '&#9650';
+			} else if (allTests[host] === 'failed'){
+				cells[host].className += 'failed ';
+				cells[host].innerHTML = 'x';
+			} 
+		});
 	}
 };
+
+/** Add click handlers to all accessor name rows, to expand test cases.
+ */
+function addRowHandlers() {
+	var rows = document.getElementsByClassName("collapsed");
+
+	for (var i = 0; i < rows.length; i++) {
+		rows[i].onclick = function() {
+			var testrows = document.getElementsByClassName(this.id);
+			if (this.classList.contains('collapsed')) {
+				this.classList.remove('collapsed');
+				this.classList.add('expanded');
+				this.children[0].innerHTML = '- ' + this.id;
+				
+				for (var j = 0; j < testrows.length; j++) {
+					testrows[j].classList.remove('hidden');
+					testrows[j].classList.add('visible');
+				}
+			
+			} else {
+				this.classList.remove('expanded');
+				this.classList.add('collapsed');
+				this.children[0].innerHTML = '+ ' + this.id;
+				
+				for (var j = 0; j < testrows.length; j++) {
+					testrows[j].classList.remove('visible');
+					testrows[j].classList.add('hidden');
+				}
+			}
+		}
+	}
+}
