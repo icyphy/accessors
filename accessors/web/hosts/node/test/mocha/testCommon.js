4afbd51c34bc86a9bd1e8da5147d94ee98ed9044
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-01-06 01:15:33 +0000

    Further work to get hierarchical Jenkins test output.

--- accessors/web/hosts/node/test/mocha/testCommon.js
+++ accessors/web/hosts/node/test/mocha/testCommon.js
@@ -7,9 +7,10 @@
 var nodeHost = require('../../nodeHost.js');
 var assert = require('assert');
 describe('NodeHost./accessors/web/hosts/node/test/mocha/testCommon', function() {
+
     var replicationMessage = 'To replicate: (cd hosts/node/test; ../../../node_modules/.bin/mocha testCommon.js)';
 
-    it('load testCommon', function () {
+    it('NodeHost./accessors/web/hosts/node/test/mocha/testCommon load testCommon', function () {
         // var testCommon = require('../../../../hosts/common/test/testCommon.js');
 	assert.equal(true, false, "Temporarily failing while we figure out commonHost.");
     });
