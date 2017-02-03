e68f7e6147dbb9323955454137cf90452ea2501b
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-02-03 20:33:43 +0000

    Added comment about what is being tested and how to test it.

--- accessors/web/hosts/node/test/testGeoCoder.js
+++ accessors/web/hosts/node/test/testGeoCoder.js
@@ -1,3 +1,23 @@
+// The problem was that if a top-level accessor sends itself an input, then
+// no reaction was being triggered. This is because
+// commonHost.provideInput() does not automatically trigger a reaction,
+// because if it did, then it would be impossible for a script to provide
+// multiple inputs to an accessor to have the accessor react to them all at
+// once.
+//
+// This strategy worked fine as long as it wasn't a _top-level_ accessor
+// sending itself an input.
+//
+// Run this in the node host, e.g., using:
+//
+//   cd accessors/web/hosts/node
+//   node nodeHostInvoke.js -js test/testGeoCoder.js -timeout 2500
+//
+// The above script should produce the latitude and longitude of
+// Berkeley on stdout:
+//  { latitude: 37.8718992, longitude: -122.2585399 }
+// Previously, it produced "undefined".
+
 var a = instantiate('a', 'services/GeoCoder');
 a.setParameter('key', 'AIzaSyBIu5hgbcSmP2f5frGdHpFNDJkDnTsFJyc');
 a.initialize();
