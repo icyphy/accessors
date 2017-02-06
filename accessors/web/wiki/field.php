ed4f457b0d8763f18b2f6ab05d146379df08dc0e
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-02-06 20:09:21 +0000

    Moved to icyphy

--- accessors/web/wiki/field.php
+++ accessors/web/wiki/field.php
@@ -1,5 +1,5 @@
 <?php 
- ini_set("include_path", "/home/www/php:/home/www/cvswww/www.terraswarm.org/accessors/wiki:.");
- include('/home/www/php/www.terraswarm.org/pmwiki/pmwiki.php');
+ ini_set("include_path", "/home/www/php:/home/www/cvswww/www.icyphy.org/accessors/wiki:.");
+ include('/home/www/php/www.icyphy.org/pmwiki/pmwiki.php');
 ?>
 
