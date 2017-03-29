1d254bad717f800e87bf601781bb38062272c6ec
eal <eal@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-03-29 00:34:34 +0000

    Fixed up AudioCapture accessor and modified ClipPlayer so that it can accept a new URL for a clip and can restart the current clip

--- trunk/accessors/web/audio/AudioCapture.js
+++ trunk/accessors/web/audio/AudioCapture.js
@@ -21,13 +21,69 @@
 // ENHANCEMENTS, OR MODIFICATIONS.
 //
 
-/** Capture audio from the microphone.
- *
+/** Capture audio from the default audio source on the host (typically the
+ *  microphone or line in).
+ *  
+ *  If the _triggered_ parameter is true (the default), then capture starts
+ *  when an event appears on the _trigger_ input port and stops after capturing
+ *  one segment of audio. Otherwise, the accessor continuously captures segments
+ *  of audio, initiating a new capture after each segment is completed.
+ *  The _captureTime_ parameter specifies the duration of each segment
+ *  of audio that is captured (in milliseconds).
+ *  
+ *  The _outputFormat_ parameter specifies the form in which the audio
+ *  should appear on the output. The available formats may include:
+ *  
+ *  * "raw": The output is a byte array representing audio data exactly as
+ *    captured in the specified capture format. This is the default and is
+ *    supported by all hosts.
+ *  * "array": The audio data is converted into arrays of numbers (one per
+ *    channel), where each number is in the range from -1.0 to 1.0.
+ *    The output is an array of arrays, where the first index specifies
+ *    the channel number.
+ *  * "samples": The audio data is converted into numbers, where
+ *    each number is in the range from -1.0 to 1.0, and each individual sample
+ *    is produced on the output as a number (if there is only one channel) or
+ *    as an array of numbers (if there is more than one channel).
+ *    Note that this format introduces quite a lot of overhead, and probably
+ *    your swarmlet will not able to capture continuous audio.
+ *  * "aiff": The audio data is converted into the AIFF file format historically
+ *    associated with Apple computers.
+ *  * "aifc": The audio data is converted into the AIFF-C, a compressed version
+ *    of AIFF.
+ *  * "au": The audio data is converted into the AU file format historically
+ *    associated with Sun Microsystems and Unix computers.
+ *  * "wav": The audio data is converted into the WAVE file format historically
+ *    associated with Windows PCs.
+ *  
+ *  The _captureFormat_ argument is an object with the following properties,
+ *  all of which are optional:
+ *  
+ *  * _bitsPerSample_: The number of bits per sample. This is an integer that
+ *    defaults to 16.
+ *  * _channels_: The number of channels. This defaults to 1.
+ *  * _sampleRate_: The sample rate. This is an integer that defaults to 8000.
+ *    Typical supported sample rates are 8000, 11025, 22050, 44100, and 48000.
+ *   
  *  This accessor requires the optional 'audio' module, which may or may
- *  not be provided by an accessor host.
+ *  not be provided by an accessor host. Moreover, a host may not support
+ *  all capture formats and all output formats.
  *
  *  @accessor audio/AudioCapture
- *  @author  Ilge Akkya
+ *  @author  Edward A. Lee and Ilge Akkya
+ *  @input trigger A trigger input for triggered mode.
+ *   The value is ignored and can be anything.
+ *  @output {Object} signal The audio output.
+ *  @parameter {int} captureTime The length of time for each audio capture
+ *   (in milliseconds).
+ *   This defaults to 1000, capturing 1 second of audio at a time.
+ *  @parameter {string} outputFormat The format of the output data representing
+ *   captured audio. This is a string that defaults to 'raw', meaning that
+ *   the raw bytes are produced on the output.
+ *  @parameter {boolean} triggered If true, use triggered mode.
+ *   Otherwise, use open-loop mode. This is a boolean that defaults to true.
+ *   This parameter is checked only upon initialization.
+
  *  @version $$Id$$
  *  @input trigger Input that triggers recording.
  *  @output {number} signal A sequence of numbers representing the captured audio signal.
@@ -39,34 +95,69 @@
 /*jshint globalstrict: true*/
 "use strict";
 
+var audio = require("audio");
+
 exports.setup = function () {
     this.input('trigger');
-    this.output('signal', {
-        'type': 'number'
+    this.parameter('captureFormat', {
+        value: {bitsPerSample: 16, channels: 1, sampleRate: 8000}
     });
-};
-
-var recorder = null;
-var handle = null;
-var audio = require("audio");
-var cacheLength = 128;
-
-var record = function () {
-    var data = recorder.get(),
-        i;
-    for (i = 0; i < data.length; i++) {
-        this.send('signal', data[i]);
+    this.parameter('captureTime', {
+        type: 'int',
+        value: 1000
+    });
+    this.parameter('outputFormat', {
+        type: 'string',
+        value: 'raw'
+    });
+    this.parameter('triggered', {
+        type: 'boolean',
+        value: true
+    });
+    this.output('signal');
+    
+    // Retrieve the supported outputFormat options from the audio module.
+    // This is in a try-catch so that this accessor can be instantiated even if the
+    // host does not provide a audio module.
+    try {
+        this.parameter('outputFormat', {
+            type: 'string',
+            value: 'raw',
+            'options': audio.outputFormats()
+        });
+    } catch (err) {
+        error(err);
     }
 };
 
+var recorder = null;
+var cacheLength = 128;
+
 exports.initialize = function () {
-    handle = this.addInputHandler("trigger", record);
-    recorder = new audio.Capture();
+    var self = this;
+    recorder = new audio.Capture(
+            self.getParameter('captureTime'),
+            self.getParameter('outputFormat'),
+            self.getParameter('captureFormat')
+    );
+    recorder.on('capture', function(audioData) {
+        self.send('signal', audioData);
+        if (self.getParameter('triggered')) {
+            recorder.stop();
+        }
+    });
+    
+    if (self.getParameter('triggered')) {
+        this.addInputHandler("trigger", function() {
+            recorder.start();
+        });
+    } else {
+        recorder.start();
+    }
 };
 
 exports.wrapup = function () {
     if (recorder !== null) {
         recorder.stop();
-        this.removeInputHandler('trigger', handle);
     }
 };
