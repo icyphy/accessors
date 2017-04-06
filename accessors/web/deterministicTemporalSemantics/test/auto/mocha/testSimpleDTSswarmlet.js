a7590cd6507d7d47a18b0efc2c9fd5b932d6f56e
chadlia.jerad <chadlia.jerad@gmail.com>
2017-04-06 21:36:31 +0000

    Updating deterministicTemporalSemantics module to support a assigning a default label, if no one has been passed + Update the demo

--- accessors/web/deterministicTemporalSemantics/test/auto/mocha/testSimpleDTSswarmlet.js
+++ accessors/web/deterministicTemporalSemantics/test/auto/mocha/testSimpleDTSswarmlet.js
@@ -15,24 +15,33 @@ describe('NodeHost' , function() {
     
     it ('NodeHost./accessors/web/mutable/test/auto/mocha/testSimpleMutable', function () {
 
-		var spAcc1 = nodeHost.instantiate('spAcc1','./deterministicTemporalSemantics/SpontaneousAcc1.js');
-		var spAcc2 = nodeHost.instantiate('spAcc1','./deterministicTemporalSemantics/SpontaneousAcc2.js');
-		var spAcc3 = nodeHost.instantiate('spAcc1','./deterministicTemporalSemantics/SpontaneousAcc3.js');
-		var spOnceAcc = nodeHost.instantiate('spAcc1','./deterministicTemporalSemantics/SpontaneousOnceAcc.js');
-		
-		var synchLabel = 'SL';
-		
-		spAcc1.setParameter('synchronizationLabel', synchLabel);
-		spAcc2.setParameter('synchronizationLabel', synchLabel);
-		spAcc3.setParameter('synchronizationLabel', synchLabel);
-		spOnceAcc.setParameter('synchronizationLabel', synchLabel);
-		
-		spAcc1.initialize();
-		spAcc2.initialize();
-		spAcc3.initialize();
-		spOnceAcc.initialize();
-		
-		//assert.equal(ma.latestOutput('scaled'), 44);
+        // Instantiating the accessors
+        var spAcc1 = nodeHost.instantiate('spAcc1','./deterministicTemporalSemantics/SpontaneousPeriodic.js');
+        var spAcc2 = nodeHost.instantiate('spAcc2','./deterministicTemporalSemantics/SpontaneousPeriodic.js');
+        var spAcc3 = nodeHost.instantiate('spAcc3','./deterministicTemporalSemantics/SpontaneousPeriodic.js');
+        var spOnceAcc = nodeHost.instantiate('spOnce','./deterministicTemporalSemantics/SpontaneousAperiodic.js');
+
+        // Setting the synchronization label
+        var synchLabel = 'SL';
+        spAcc1.setParameter('synchronizationLabel', synchLabel);
+        spAcc2.setParameter('synchronizationLabel', synchLabel);
+        spAcc3.setParameter('synchronizationLabel', synchLabel);
+        spOnceAcc.setParameter('synchronizationLabel', synchLabel);
+
+        // Setting the periods and timeouts
+        spAcc1.setParameter('period', 2000);
+        spAcc2.setParameter('period', 4000);
+        spAcc3.setParameter('period', 1200);
+        spOnceAcc.setParameter('timeout', 1500);
+
+        // Initializing the spontaneous accessors
+        spAcc1.initialize();
+        spAcc2.initialize();
+        spAcc3.initialize();
+        spOnceAcc.initialize();
+
+        // Note: The periodic accessors will stop at 12000 by clearing the timers.  
+
 
     });
 });
