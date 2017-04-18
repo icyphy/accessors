f301fe862b43f1ea4e56f71079a9ebc4d08ef5cb
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-04-18 02:21:24 +0000

    Added ImageDisplay for Cape Code and Node and Cameras for Node.

---
+++ trunk/accessors/web/cameras/demo/Capture.js
@@ -0,0 +1,29 @@
+exports.setup = function() {
+    //  This composite accessor was created by Cape Code.
+    //  To run the code, run: 
+    //  (cd $PTII/org/terraswarm/accessor/accessors/web/cameras/demo; node ../../hosts/node/nodeHostInvoke.js cameras/demo/Capture)
+    //  To regenerate this composite accessor, run:
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/Capture.xml
+    //  to edit the model, run:
+    //  $PTII/bin/vergil -capecode $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/Capture.xml
+
+    // Ports: Capture: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+
+    // Start: Camera: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var Camera = this.instantiate('Camera', 'cameras/Camera.js');
+    Camera.setParameter('camera', "default camera");
+    Camera.setParameter('viewSize', {"width":640, "height":480});
+    Camera.setParameter('triggered', true);
+
+    // Start: ImageDisplay: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var ImageDisplay = this.instantiate('ImageDisplay', 'image/ImageDisplay.js');
+
+    // Start: TestSpontaneous: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
+    var TestSpontaneous = this.instantiate('TestSpontaneous', 'test/TestSpontaneous.js');
+    TestSpontaneous.setParameter('interval', 100.0);
+
+    // Connections: Capture: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
+    this.connect(TestSpontaneous, 'output', Camera, 'trigger');
+    this.connect(Camera, 'image', ImageDisplay, 'input');
+};
+this.stopAt(20000.0);
---
+++ trunk/accessors/web/image/ImageDisplay.js
@@ -0,0 +1,58 @@
+// Display an image on the console.
+//
+// Copyright (c) 2016 The Regents of the University of California.
+// All rights reserved.
+//
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+//
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+//
+
+/** Display an image on the console.
+ *
+ *  @accessor image/ImageDisplay
+ *  @param input The output
+ *  @author Christopher Brooks
+ *  @version $$Id$$
+ */
+
+// Stop extra messages from jslint.  Note that there should be no
+// space between the / and the * and global.
+/*globals console, error, exports, require */
+/*jshint globalstrict: true*/
+"use strict";
+
+var imageDisplay = require('imageDisplay');
+var display = null;
+
+exports.setup = function () {
+    this.input('input');
+    this.output('output');
+};
+
+exports.initialize = function () {
+    if (display === null) {
+        display = new imageDisplay.ImageDisplay();
+    }
+
+    this.addInputHandler('input', function () {
+        var inputValue = this.get('input');
+        display.displayImage(inputValue);
+        this.send('output', inputValue);
+    });
+};
