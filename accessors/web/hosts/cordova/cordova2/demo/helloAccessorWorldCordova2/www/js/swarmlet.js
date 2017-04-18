e0c0c9c525af29393f5eb6b843c97a9c1ed4b58e
chadlia.jerad <chadlia.jerad@gmail.com>
2017-04-18 06:24:06 +0000

    added another cordova host (cordova2), check the wikipage for the use

---
+++ accessors/web/hosts/cordova/cordova2/demo/helloAccessorWorldCordova2/www/js/swarmlet.js
@@ -0,0 +1,51 @@
+// Cordova host swarmlet test
+//
+// Copyright (c) 2016-2017 The Regents of the University of California.
+// All rights reserved.
+//
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+//
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+//
+
+/** This "swarmlet" example, running on Cordova, illustrates the use of Cordova
+ *  Host. The swarmlet loads loads a 'testAcc', initializes it, provides an input
+ *  and then diplays the output after caling react().
+ *  
+ *  See https://www.icyphy.org/accessors/wiki/Main/CordovaHost2
+ *  
+ *  @module swarmlet.js
+ *  @author Chadlia Jerad
+ *  @version $$Id: swarmlet.js 1502 2017-04-17 21:34:03Z cxh $$
+ */
+
+var a1 = instantiate('a1','./js/accessors/testAcc.js');
+
+MobileLog('Accessor "a1" instantiated as an: '+typeof(a1)); 
+
+a1.initialize();
+                        
+MobileLog('Accessor "a1" initialized'); 
+
+a1.provideInput('input', 5);
+
+MobileLog('Accessor "a1" provided input:5'); 
+                        
+a1.react();
+        
+MobileLog('Latest output of ' + a1.accessorName + ': ' + a1.latestOutput('scaled'));
