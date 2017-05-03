e361ecf1f9c76c59b524c1df330621dff68cb7ef
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-05-03 14:03:06 +0000

    Updated after reloading and regenerating code.

--- accessors/web/cameras/demo/CaptureOpenLoop.js
+++ accessors/web/cameras/demo/CaptureOpenLoop.js
@@ -1,11 +1,11 @@
 exports.setup = function() {
     //  This composite accessor was created by Cape Code.
     //  To run the code, run: 
-    //  (cd $PTII/org/terraswarm/accessor/accessors/web/cameras/demo; node ../../hosts/node/nodeHostInvoke.js cameras/demo/CaptureOpenLoop)
+    //  (cd $PTII/org/terraswarm/accessor/accessors/web/cameras/demo; node ../../node_modules/@accessors-hosts/node/nodeHostInvoke.js cameras/demo/CaptureOpenLoop)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/CaptureOpenLoop.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/CaptureOpenLoop.xml
     //  to edit the model, run:
-    //  $PTII/bin/vergil -capecode $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/CaptureOpenLoop.xml
+    //  $PTII/bin/capecode $PTII/./ptolemy/actor/lib/jjs/modules/cameras/demo/Capture/CaptureOpenLoop.xml
 
     // Ports: CaptureOpenLoop: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
