ebae315997e8c37412109f4fb101ec6ba6ced333
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-05-09 16:59:18 +0000

    Renamed cleanAll to clean.

--- accessors/web/net/test/auto/WebSocketClient3JS.js
+++ accessors/web/net/test/auto/WebSocketClient3JS.js
@@ -3,9 +3,9 @@ exports.setup = function() {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/net/test/auto; node ../../../node_modules/@accessors-hosts/node/nodeHostInvoke.js net/test/auto/WebSocketClient3JS)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/./org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
+    //  $PTII/bin/capecode $PTII/org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
 
     // Ports: WebSocketClient3JS: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
@@ -59,10 +59,10 @@ if (exports.initialize) {
     var originalInitialize = exports.initialize;
     exports.initialize = function() {
         originalInitialize();
-        this.stopAt(20000.0);
+        this.stopAt(16000.0);
     }
 } else {
     exports.initialize = function() {
-        this.stopAt(20000.0);
+        this.stopAt(16000.0);
     }
 }
