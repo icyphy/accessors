ce66d69e4b95bb5705ce8be754876793aad47339
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-05-09 16:59:18 +0000

    Renamed cleanAll to clean.

--- accessors/web/net/test/auto/WebSocketClientJS.js
+++ accessors/web/net/test/auto/WebSocketClientJS.js
@@ -3,9 +3,9 @@ exports.setup = function() {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/net/test/auto; node ../../../node_modules/@accessors-hosts/node/nodeHostInvoke.js net/test/auto/WebSocketClientJS)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./org/terraswarm/accessor/test/auto/WebSocketClientJS.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/org/terraswarm/accessor/test/auto/WebSocketClientJS.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/./org/terraswarm/accessor/test/auto/WebSocketClientJS.xml
+    //  $PTII/bin/capecode $PTII/org/terraswarm/accessor/test/auto/WebSocketClientJS.xml
 
     // Ports: WebSocketClientJS: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
