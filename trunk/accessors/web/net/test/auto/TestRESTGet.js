36f26aad648ece033ebaab44cb7099c43b0bbaa4
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-05-13 15:43:15 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- trunk/accessors/web/net/test/auto/TestRESTGet.js
+++ trunk/accessors/web/net/test/auto/TestRESTGet.js
@@ -3,9 +3,9 @@ exports.setup = function() {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/net/test/auto; node ../../../node_modules/@accessors-hosts/node/nodeHostInvoke.js  net/test/auto/TestRESTGet)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/actor/lib/jjs/modules/httpClient/test/auto/TestRESTGet.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./ptolemy/actor/lib/jjs/modules/httpClient/test/auto/TestRESTGet.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/ptolemy/actor/lib/jjs/modules/httpClient/test/auto/TestRESTGet.xml
+    //  $PTII/bin/capecode $PTII/./ptolemy/actor/lib/jjs/modules/httpClient/test/auto/TestRESTGet.xml
 
     // Ports: TestRESTGet: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
