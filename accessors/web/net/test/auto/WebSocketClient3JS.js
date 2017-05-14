9f3bb7b5d56d92f4a65a044f4377c6597861c6ce
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-05-14 10:08:26 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- accessors/web/net/test/auto/WebSocketClient3JS.js
+++ accessors/web/net/test/auto/WebSocketClient3JS.js
@@ -3,9 +3,9 @@ exports.setup = function() {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/net/test/auto; node ../../../node_modules/@accessors-hosts/node/nodeHostInvoke.js net/test/auto/WebSocketClient3JS)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
+    //  $PTII/bin/capecode $PTII/./org/terraswarm/accessor/test/auto/WebSocketClient3JS.xml
 
     // Ports: WebSocketClient3JS: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
