25a27d0c6d24ca56227e91cd616e27812afbbf1d
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-05-17 23:31:08 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- trunk/accessors/web/net/test/auto/TCPSocketUnsignedShort.js
+++ trunk/accessors/web/net/test/auto/TCPSocketUnsignedShort.js
@@ -3,9 +3,9 @@ exports.setup = function() {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/net/test/auto; node ../../../node_modules/@accessors-hosts/node/nodeHostInvoke.js net/test/auto/TCPSocketUnsignedShort)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/org/terraswarm/accessor/test/auto/TCPSocketUnsignedShort.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./org/terraswarm/accessor/test/auto/TCPSocketUnsignedShort.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/org/terraswarm/accessor/test/auto/TCPSocketUnsignedShort.xml
+    //  $PTII/bin/capecode $PTII/./org/terraswarm/accessor/test/auto/TCPSocketUnsignedShort.xml
 
     // Ports: TCPSocketUnsignedShort: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
