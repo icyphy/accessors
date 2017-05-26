e516ca4251ef1031dfce891fe6d1c3ddec45eea2
Hokeun Kim <2585943+hokeun@users.noreply.github.com>
2017-05-26 18:19:21 +0000

    initial version of httpClient module for Cordova host

---
+++ accessors/web/hosts/cordova/cordova2/demo/helloAccessorWorldCordova2/www/js/modules/httpClient/httpClient.js
@@ -0,0 +1,81 @@
+// Below is the copyright agreement for the Ptolemy II system.
+//
+// Copyright (c) 2015-2016 The Regents of the University of California.
+// All rights reserved.
+//
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+//
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+//
+//
+// Ptolemy II includes the work of others, to see those copyrights, follow
+// the copyright link on the splash page or see copyright.htm.
+/**
+ * Module for HTTP clients.
+ * FIXME: This currently only supports a get request.
+ * <pre>
+ * httpClient.get({
+ *      trustAll: true,
+ *      url: "https://www.google.com"
+ *  },
+ *  function(data) {
+ *      MobileLog(data);
+ *  }
+ * );
+ * </pre>
+ * Both http and https are supported.
+ *
+ * @module httpClient
+ * @author Hokeun Kim and Matt Weber
+ * @version $$Id: httpClient.js 75980 2017-04-23 00:19:25Z beth@berkeley.edu $$
+ */
+
+var EventEmitter = require('events').EventEmitter;
+
+/** Issue an HTTP get request
+ *  The options argument should be a map with the following fields
+ *  <ul>
+ *  <li> trustAll: Trust all certificates.
+ *  <li> url: A string that can be parsed as a URL.
+ *  </ul>
+ */
+var httpClient = {
+    get: function (options, responseCallback) {
+        if (options.trustAll === true) {
+            cordovaHTTP.acceptAllCerts(true, function() {
+                MobileLog('accepting all certs success!');
+            }, function() {
+                MobileLog('error :(');
+            });
+        }
+
+        cordovaHTTP.get(options.url, {
+            id: 12,
+            message: "test"
+        }, { Authorization: "OAuth2: token" }, function(response) {
+            responseCallback(response.data);
+            //MobileLog(response.status);
+            //MobileLog(response.data);
+        }, function(response) {
+            responseCallback(response.error);
+            //MobileLog(response.error);
+        });
+    }
+};
+
+exports.httpClient = httpClient;
