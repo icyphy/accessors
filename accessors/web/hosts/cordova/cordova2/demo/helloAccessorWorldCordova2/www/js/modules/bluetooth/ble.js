3c845ae98793162d1bb2a5cb07f5f2c45d95d969
victor.nouvellet <victor.nouvellet@insa-lyon.fr>
2017-05-26 18:39:57 +0000

    Add ble.js module

---
+++ accessors/web/hosts/cordova/cordova2/demo/helloAccessorWorldCordova2/www/js/modules/bluetooth/ble.js
@@ -0,0 +1,86 @@
+// Below is the copyright agreement for the Ptolemy II system.
+//
+// Copyright (c) 2015-2016 The Regents of the University of California.
+// All rights reserved.
+//
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+//
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+//
+//
+// Ptolemy II includes the work of others, to see those copyrights, follow
+// the copyright link on the splash page or see copyright.htm.
+/**
+ * Module for BLE discovery and connection.
+ *
+ * @module ble
+ * @author Chadlia Jerad, Victor Nouvellet
+ * @version $$Id: ble.js 75980 2017-04-23 00:19:25Z victor.nouvellet@berkeley.edu $$
+ */
+
+
+var ble = {
+    startScan: function (successCallback, errorCallback, options) {
+        // Scan for all services.
+        var foundDevices = {};
+
+        evothings.ble.startScan(
+            function(device)
+            {
+                console.log('startScan found device named: ' + device.name);
+                var count = Object.size(foundDevices);
+                foundDevices[device.address] = device;
+                if (Object.size(foundDevices) > count || options.allowDuplicates == true) {
+                    // New device -> call callback
+                    successCallback(device);
+                } 
+            },
+            function(errorCode)
+            {
+                console.log('startScan error: ' + errorCode);
+                errorCallback(errorCode);
+            }
+        );
+    },
+    stopScan: function () {
+        evothings.ble.stopScan();
+    },
+    connectToDevice: function (devcie, onConnected, onDisconnected, onConnectError, options) {
+        evothings.ble.stopScan();
+    },
+    getService: function (device, uuid) {
+        evothings.ble.getService(device, uuid);
+    },
+    getCharacteristic: function (service, uuid) {
+        evothings.ble.getCharacteristic(service, uuid);
+    },
+    getDescriptor: function (characteristic, uuid) {
+        evothings.ble.getDescriptor(characteristic, uuid);
+    },
+    readCharacteristic: function (device, characteristic, success, fail) {
+        evothings.ble.readCharacteristic(device, characteristic, success, fail);
+    },
+    writeCharacteristic: function (device, characteristic, data, success, fail) {
+        evothings.ble.writeCharacteristic(device, characteristic, data, success, fail);
+    },
+    enableNotification: function (device, characteristic, success, fail) {
+        evothings.ble.enableNotification(device, characteristic, success, fail);
+    }
+};
+
+exports.ble = ble;
