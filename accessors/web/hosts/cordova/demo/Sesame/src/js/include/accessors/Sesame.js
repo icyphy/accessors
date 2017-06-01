d077eab646906fa572458a19e253cbc8bcf69367
marten <19938940+lhstrh@users.noreply.github.com>
2017-06-01 21:51:35 +0000

    Sesame demo.

---
+++ accessors/web/hosts/cordova/demo/Sesame/src/js/include/accessors/Sesame.js
@@ -0,0 +1,54 @@
+// Cordova host swarmlet test
+//
+// Copyright (c) 2016-2017 The Regents of the University of California.
+// All rights reserved.
+//
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
+//
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+//
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
+//
+
+/** This "swarmlet" example, running on Cordova, illustrates the use of Cordova
+ *  Host. The swarmlet loads loads a 'testAcc', initializes it, provides an input
+ *  and then diplays the output after caling react().
+ *  
+ *  See https://www.icyphy.org/accessors/wiki/Main/CordovaHost2
+ *  
+ *  @module swarmlet.js
+ *  @author Chadlia Jerad, Victor Nouvellet
+ *  @version $$Id: swarmlet.js 1502 2017-04-17 21:34:03Z cxh $$
+ */
+
+exports.setup = function() {
+    console.log('setup of bleDoorOpening');
+
+    var trigger = this.instantiate('trigger', 'TestSpontaneousOnce');
+    var door = instantiate('door','Door');
+    var ble = instantiate('ble','bleAccessor');
+
+    trigger.setParameter('delay', 1000.0);
+    trigger.setParameter('value', true);
+
+    this.connect(trigger, 'trigger', ble, 'startScan');
+    this.connect(ble, '...', door, 'trigger');
+};
+
+exports.initialize = function () {
+  console.log('test1 init top level ');
+  //this.addInputHandler('trigger', triggerInputHandler);
+};
