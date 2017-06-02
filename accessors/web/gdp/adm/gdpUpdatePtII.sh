aeb6f52a86d6ee3a5d1231bf055df5f5c1cff701
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-06-02 02:37:28 +0000

    Make it more obvious when we are trying different hosts.

--- accessors/web/gdp/adm/gdpUpdatePtII.sh
+++ accessors/web/gdp/adm/gdpUpdatePtII.sh
@@ -7,6 +7,7 @@ if [ "`uname -s`" != "Darwin" ]; then
     echo "$0: Must be run under Mac OS X because we need to build there and then ssh to RHEL and Linux."
 fi
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@@@: $0: About to build locally"
 gdpVersion=`cat $PTII/vendors/gdp/gdp/git-version.txt`
 # Build locally
@@ -14,7 +15,8 @@ export PKGMGR=brew
 $PTII/org/terraswarm/accessor/accessors/web/gdp/adm/gdpBuildDevelEnvironment.sh
 (cd $PTII/lib; svn commit -m "Updated gdp libraries on Darwin to $gdpVersion" libgdp*)
 
-# echo "@@@@: $0: About to build on terra"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "@@@@: $0: About to build on terra"
 terraPTII=/home/cxh/src/ptII
 terraAccessors=$terraPTII/org/terraswarm/accessor/accessors/web
 # # Update so that we get the Darwin libraries to be included in the GDP jar file and eventually the @terraswarm/accessors module.
@@ -23,6 +25,7 @@ ssh terra "(cd $terraAccessors; svn update --accept theirs-full)"
 ssh terra "$terraAccessors/gdp/adm/gdpBuildDevelEnvironment.sh"
 ssh terra "(cd $terraPTII/lib; svn commit -m \"Updated gdp libraries on RHEL to $gdpVersion\" libgdp* linux-x86-64-rhel)"
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@@@: $0: About to build on swarmnuc001"
 ubuntuPTII=/home/cxh/ptII
 ubuntuAccessors=$ubuntuPTII/org/terraswarm/accessor/accessors/web
@@ -32,10 +35,12 @@ ssh swarmnuc001 "(cd $ubuntuAccessors; svn update --accept theirs-full)"
 ssh swarmnuc001 "export PTII=$ubuntuPTII; $ubuntuAccessors/gdp/adm/gdpBuildDevelEnvironment.sh"
 ssh swarmnuc001 "(cd $ubuntuPTII/lib; svn commit -m \"Updated gdp libraries on Ubuntu to $gdpVersion\" libgdp* linux-x86-64-rhel)"
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@@@: $0: Rebuilding locally to get updated libraries"
-#(cd $PTII/lib; svn update --accept theirs-full)
-#$PTII/org/terraswarm/accessor/accessors/web/gdp/adm/gdpBuildDevelEnvironment.sh
+(cd $PTII/lib; svn update --accept theirs-full)
+$PTII/org/terraswarm/accessor/accessors/web/gdp/adm/gdpBuildDevelEnvironment.sh
 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "@@@@: $0:  Get the most recent package.json and update the version number"
 (cd $PTII/vendors/gdp/gdp; git checkout lang/js/package.json)
 awk '{ if ($0 ~ /"version":/) {
