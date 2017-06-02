e7da9c87bdd0239acb09f2e3144cc2d690b75172
cxh <cxh@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-06-02 12:36:26 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- trunk/accessors/web/test/auto/RampJSTest.js
+++ trunk/accessors/web/test/auto/RampJSTest.js
@@ -13,14 +13,14 @@ exports.setup = function() {
     var TestSpontaneous = this.instantiate('TestSpontaneous', 'test/TestSpontaneous.js');
     TestSpontaneous.setParameter('interval', 1000.0);
 
-    // Start: JavaScriptRamp: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
+    // Start: JavaScript Ramp With Spaces In Its Name: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/lib/jjs/JavaScript.java
     // FIXME: See instantiate() in accessors/web/hosts/common/commonHost.js
     // We probably need to do something with the bindings.
-    var JavaScriptRamp = new Accessor('JavaScriptRamp', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id$$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValue = 0;\n\nexports.initialize = function() {\n    _lastValue = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValue += this.getParameter(\'step\');\n    this.send(\'output\', _lastValue);\n};', null, null, null, null);
-    JavaScriptRamp.container = this;
-    this.containedAccessors.push(JavaScriptRamp);
-    JavaScriptRamp.setParameter('init', 0.0);
-    JavaScriptRamp.setParameter('step', 1.0);
+    var JavaScript_Ramp_With_Spaces_In_Its_Name = new Accessor('JavaScript_Ramp_With_Spaces_In_Its_Name', '/** Output a sequence with a given step in values.\n *\n *  @accessor test/TestRamp\n *  @param init The value produced on its first iteration.  The\n *  initial default is 0.\n *  @input trigger The trigger\n *  @output output The output\n *  @param step The amount by which the output is incremented. The\n *  default is 1.\n *  @author Christopher Brooks\n *  @version $$Id$$\n */\nexports.setup = function() {\n    // FIXME: this only supports numbers, unlike the Cape Code Ramp\n    // actor, which supports many types.\n    this.parameter(\'init\', {\'type\':\'number\', \'value\':0});\n    this.input(\'trigger\');\n    this.output(\'output\', {\'type\':\'number\'});\n    this.parameter(\'step\', {\'type\':\'number\', \'value\':1});\n};\n\nvar _lastValue = 0;\n\nexports.initialize = function() {\n    _lastValue = this.getParameter(\'init\');\n}\nexports.fire = function() {\n    _lastValue += this.getParameter(\'step\');\n    this.send(\'output\', _lastValue);\n};', null, null, null, null);
+    JavaScript_Ramp_With_Spaces_In_Its_Name.container = this;
+    this.containedAccessors.push(JavaScript_Ramp_With_Spaces_In_Its_Name);
+    JavaScript_Ramp_With_Spaces_In_Its_Name.setParameter('init', 0.0);
+    JavaScript_Ramp_With_Spaces_In_Its_Name.setParameter('step', 1.0);
 
     // Start: Stop: ptolemy/cg/adapter/generic/accessor/adapters/org/terraswarm/accessor/JSAccessor.java
     var Stop = this.instantiate('Stop', 'utilities/Stop.js');
@@ -32,9 +32,9 @@ exports.setup = function() {
     TrainableTest2.setParameter('tolerance', 1.0E-9);
 
     // Connections: RampJSTest: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
-    this.connect(TestSpontaneous, 'output', JavaScriptRamp, 'trigger');
+    this.connect(TestSpontaneous, 'output', JavaScript_Ramp_With_Spaces_In_Its_Name, 'trigger');
     this.connect(TrainableTest2, 'output', Stop, 'stop');
-    this.connect(JavaScriptRamp, 'output', TrainableTest2, 'input');
+    this.connect(JavaScript_Ramp_With_Spaces_In_Its_Name, 'output', TrainableTest2, 'input');
 };
 
 // To update the initialize code below, modify
