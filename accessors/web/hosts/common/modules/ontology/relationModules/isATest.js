6313176446203c4f929af85b733ee0cfd3e228dc
matt.weber <mew2ub@users.noreply.github.com>
2017-06-12 22:16:21 +0000

    refactored relations out of ontology folder

---
+++ accessors/web/hosts/common/modules/ontology/relationModules/isATest.js
@@ -0,0 +1,12 @@
+exports.isA = function(args){
+    if(args.length != 2){
+      error("isA has an incorrect number of arguments");
+    }
+    if(args[0] == "nonentity"){
+      return false;
+    } else if(args[1] == "entity"){
+      return true;
+    } else {
+      return false;
+    }  
+}
