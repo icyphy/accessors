505f7473af6951d9bac6066c6b25a31a380785c6
matt.weber <mew2ub@users.noreply.github.com>
2017-06-12 20:22:49 +0000

    Added work in progress on a parser for a logic language for ontologies. The parser in ontologyLogicParser.js is generated from ontologyLogicParser.pegjs via the PEG.js tool, so they both go in the ontology folder.

---
+++ accessors/web/hosts/common/modules/ontology/relations.js
@@ -0,0 +1,58 @@
+exports.interpret = function (name, args){
+    switch(name){
+      case "isA":
+        return isA(args);
+        break;
+      case "hasA":
+        return hasA(args);
+        break;
+      case "close":
+        return close(args);
+        break;
+      case "closed":
+        return closed(args);
+        break;
+      case "getType":
+        return getType(args);
+      default:
+        error("invalid relation: " + relat)
+    }
+}
+
+function isA(args) {
+    if(args.length != 2){
+      error("isA has an incorrect number of arguments");
+    }
+    if(args[0] == "nonentity"){
+      return false;
+    } else if(args[1] == "entity"){
+      return true;
+    } else {
+      return false;
+    }
+}
+
+function hasA(args) {
+    if(args.length != 2){
+      error("hasA has an incorrect number of arguments");
+    }
+    if(args[0] == "nothing"){
+      return false;
+    } else if(args[1] == "everything"){
+      return true;
+    } else{
+      return false;
+    }
+}
+
+function close(args){
+    return true;
+}
+
+function closed(args){
+    return false;
+}
+
+function getType(args){
+    return typeof args[0];
+}
