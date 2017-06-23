dc5fd7e7c02fb7d01cda59f98786e66ced2db310
Edward A. Lee <8513334+edwardalee@users.noreply.github.com>
2017-06-23 21:43:54 +0000

    Added resources input to provide resources (such as images) that the server can serve

--- accessors/web/utilities/Browser.js
+++ accessors/web/utilities/Browser.js
@@ -30,6 +30,11 @@
  *
  *  @accessor utilities/Browser
  *  @input {string} html An HTML document to render in the browser.
+ *  @input resources An object where each named property is an object containing
+ *   two properties, 'data' and 'contentType'. The name of the named property is
+ *   the path to be used to access the resource. The 'data' property is the resource
+ *   itself, an arbitrary collection of bytes. The 'contentType' is the MIME
+ *   type of the data.
  *  @author Edward A. Lee (eal@eecs.berkeley.edu)
  *  @version $$Id$$
  */
@@ -47,15 +52,30 @@ exports.setup = function () {
     this.input('html', {
         'type': 'string'
     });
+    this.input('resources');
 };
 
 var display = function () {
+    // Check for any new resources.
+    var resources = this.get('resources');
+    if (resources) {
+        for (var name in resources) {
+            browser.addResource(name, resources[name].data, resources[name].contentType);
+        }
+    }
+
     var toDisplay = this.get('html');
     browser.display(toDisplay);
 };
 
 exports.initialize = function () {
     this.addInputHandler('html', display);
+    this.addInputHandler('resources', function() {
+        var resources = this.get('resources');
+        for (var name in resources) {
+            browser.addResource(name, resources[name].data, resources[name].contentType);
+        }
+    });
 };
 
 exports.wrapup = function () {
