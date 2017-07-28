05a3fa72076d2717918bd60e1ac6ed89950d0ea9
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-07-28 12:05:43 +0000

    Moved description of the output to the body of the class comment because jsdoc was having problems.

--- accessors/web/services/WiFiScan.js
+++ accessors/web/services/WiFiScan.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2017 The Regents of the University of California.
+// Copyright (c) 2017 The Regents of the University of California.
 // All rights reserved.
 //
 // Permission is hereby granted, without written agreement and without
@@ -22,23 +22,28 @@
 //
 
 /** Obtain a scan of nearby WiFi networks. 
-*
-*   @accessor services/WiFiScan
-*   @input trigger Initiates a WiFiScan. 
-*   @output wifiData The list of networks obtained from the wifi-scanner module. The output is a JSON array of objects in the form:
-*   networks = [
-*        {   "level": signal_level, // raw RSSI value
-*            "SSID": ssid, // SSID as string, with escaped double quotes: "\"ssid name\""
-*            "BSSID": bssid // MAC address of WiFi router as string
-*            "frequency": frequency of the access point channel in MHz
-*            "capabilities": capabilities // Describes the authentication, key management, and encryption schemes supported by the access point.
-*        }
-*    ]
-*
-*  
-*  @author Matt Weber
-*  @version $$Id: swarmlet.js 1502 2017-04-17 21:34:03Z cxh $$
-*/
+ *
+ *  The output is in the form below:
+ *  ```
+ *   networks = [
+ *        {   "level": signal_level, // raw RSSI value
+ *            "SSID": ssid, // SSID as string, with escaped double quotes: "\"ssid name\""
+ *            "BSSID": bssid // MAC address of WiFi router as string
+ *            "frequency": frequency of the access point channel in MHz
+ *            "capabilities": capabilities // Describes the authentication, key management, and encryption schemes supported by the access point.
+ *        }
+ *    ]
+ *  ```
+ *
+ *  @accessor services/WiFiScan
+ *  @input trigger Initiates a WiFiScan. 
+ *  @output wifiData The list of networks obtained from the
+ *  wifi-scanner module. The output is a JSON array of objects shown
+ *  above.
+ *  
+ *  @author Matt Weber
+ *  @version $$Id: swarmlet.js 1502 2017-04-17 21:34:03Z cxh $$
+ */
 
 // Stop extra messages from jslint and jshint.  Note that there should
 // be no space between the / and the * and global. See
