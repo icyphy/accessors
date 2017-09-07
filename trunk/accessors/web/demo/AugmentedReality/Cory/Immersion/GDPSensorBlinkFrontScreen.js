3645bfb1921465bc488711676994eaf46eca0d40
vc@icyphy.org <vc@icyphy.org@7f790284-b32a-46e3-b8ea-16a5cb6626fa>
2017-09-07 22:37:35 +0000

    Working on getting gdp accessor code to download.

---
+++ trunk/accessors/web/demo/AugmentedReality/Cory/Immersion/GDPSensorBlinkFrontScreen.js
@@ -0,0 +1,30 @@
+// Stop extra messages from jslint. Note that there should be no               
+// space between the / and the * and global.                         
+/*globals console, error, exports, require */
+/*jshint globalstrict: true*/
+"use strict";
+
+exports.setup = function() {
+    this.output('data', {'type' : 'JSON'});
+
+    var GDPLogSubscribe = this.instantiate('GDPLogSubscribe', 'gdp/GDPLogSubscribe');
+    GDPLogSubscribe.input('logname', {
+	    'value':'edu.berkeley.eecs.swarmlab.device.C098E590000A',
+		});
+
+    var code = "\                                       
+    exports.setup = function() {\n\                            
+      this.input('in');\n\                               
+      this.output('out');\n\                              
+  }\n\                                           
+  exports.initialize = function() {\n\                           
+      var thiz = this;\n\                                
+      this.addInputHandler('in', function() {\n\                    
+          thiz.send('out', JSON.parse(thiz.get('in')));\n\             
+      });\n\                                      
+  }";
+    var JavaScript = this.instantiateFromCode('JavaScript', code, false);
+
+    this.connect(JavaScript, 'out', 'data');
+    this.connect(GDPLogSubscribe, 'data', JavaScript, 'in');
+};
