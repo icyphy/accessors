efe40ec01a5b105ae1d7cf8687f5455ffb2d3c51
marten <19938940+lhstrh@users.noreply.github.com>
2017-09-08 21:45:42 +0000

    Working on BLEAdvertise plugin.

--- accessors/web/hosts/cordova/plugins/BLEAdvertise/src/android/BLEAdvertise.java
+++ accessors/web/hosts/cordova/plugins/BLEAdvertise/src/android/BLEAdvertise.java
@@ -7,13 +7,34 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.uriio.beacons.Beacons;
+import java.util.HashMap;
+
+
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class BLEAdvertise extends CordovaPlugin {
 
+    private HashMap<Integer, Beacon> activeBeacons = new HashMap<Integer, Beacon>();
+    private counter = 0;
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, 
+        CallbackContext callbackContext) throws JSONException {
+        switch (action) {
+            case "initialize":
+                this.initialize();
+                break;
+            case "startEddystoneURLBeacon":
+                String url = args.getString(0);
+                this.startEddystoneURLBeacon();
+                break;
+            default:
+                throw new IllegalArgumentException("No such method: " + action);
+        }
+        // FIXME: example code, remove.
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
@@ -28,5 +49,28 @@ public class BLEAdvertise extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    /**
+     * Initialize the BLE beacon advertising library.
+     */
+    private void initialize() {
+        // Don't initialize twice.
+        try {
+            Beacons.getInstance();
+        } catch(RuntimeException e) {
+            Beacons.initialize(this);    
+        }
+    }
+
+     /**
+     * Initialize the BLE beacon advertising library.
+     */
+    private void startEddystoneURLBeacon(String url, CallbackContext callbackContext) {
+        // FIXME: perhaps just do initialize here, and remove it from the interface.
+        Beacon beacon = new EddystoneURL(url).start();
+        int handle = this.counter++;
+        this.activeBeacons.add(handle, beacon);
+        callbackContext.success(handle);
     }
 }
