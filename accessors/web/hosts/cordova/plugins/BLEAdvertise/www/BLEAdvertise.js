644cc939ed4acec3210cd12d1a17c1d3d2ee13ab
marten <19938940+lhstrh@users.noreply.github.com>
2017-09-08 21:45:42 +0000

    Working on BLEAdvertise plugin.

--- accessors/web/hosts/cordova/plugins/BLEAdvertise/www/BLEAdvertise.js
+++ accessors/web/hosts/cordova/plugins/BLEAdvertise/www/BLEAdvertise.js
@@ -1,12 +1,38 @@
 var exec = require('cordova/exec');
 
-
-// Example
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "BLEAdvertise", "coolMethod", [arg0]);
-};
-
-
+/** Initialize the beacons-android library. */
 exports.initialize = function(success, error) {
     exec(success, error, "BLEAdvertise", "initialize", []);
+};
+
+/**
+ * Stop advertisement of all beacons.
+ */
+exports.stopAll = function() {
+	var error = function() {
+		console.log("Error while stopping advertisement.");
+	}
+	exec(null, error, "BLEAdvertise", "stopAll", []);
+}
+
+/**
+ * Stop advertisement of particular beacon.
+ */
+exports.stop = function(handle) {
+	var error = function() {
+		console.log("Error while stopping advertisement.");
+	}
+	exec(null, error, "BLEAdvertise", "stop", []);
+}
+
+/** 
+ * Start advertising Eddystone-URL beacon.
+ * Upon success, the supplied callback is invoked with one argument.
+ * The argument is a handle that can be used to stop the advertisement.
+ */
+exports.startEddystoneURLBeacon = function(URL, callback) {
+	var error = function() {
+		console.log("Error starting advertisement.");
+	}
+    exec(callback, error, "BLEAdvertise", "startEddystoneURLBeacon", [URL]);
 };
