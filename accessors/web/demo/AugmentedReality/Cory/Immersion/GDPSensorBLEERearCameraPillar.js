30575d48fc5f7bc8e9c623d9d8734e9f2fcf6e4c
vc <vc@icyphy.org>
2017-09-08 01:47:13 +0000

    Use the testLog because the other GDP feeds don't work.

--- accessors/web/demo/AugmentedReality/Cory/Immersion/GDPSensorBLEERearCameraPillar.js
+++ accessors/web/demo/AugmentedReality/Cory/Immersion/GDPSensorBLEERearCameraPillar.js
@@ -9,20 +9,21 @@ exports.setup = function() {
 
     var GDPLogSubscribe = this.instantiate('GDPLogSubscribe', 'gdp/GDPLogSubscribe');
     GDPLogSubscribe.input('logname', {
-	    'value':'edu.berkeley.eecs.swarmlab.device.c098e5300003'
+	    // 'value':'edu.berkeley.eecs.swarmlab.device.c098e5300003'
+	    'value':'edu.berkeley.eecs.testlog'
 		});
 
-    var code = "\                                       
-    exports.setup = function() {\n\                            
-      this.input('in');\n\                               
-      this.output('out');\n\                              
-  }\n\                                           
-  exports.initialize = function() {\n\                           
-      var thiz = this;\n\                                
-      this.addInputHandler('in', function() {\n\                    
-          thiz.send('out', JSON.parse(thiz.get('in')));\n\             
-      });\n\                                      
-  }";
+    var code = "\
+    exports.setup = function() {\n\
+      this.input('in');\n\
+      this.output('out');\n\
+    }\n\
+    exports.initialize = function() {\n\
+      var thiz = this;\n\
+      this.addInputHandler('in', function() {\n\
+          thiz.send('out', JSON.parse(thiz.get('in')));\n\
+      });\n\
+    }";
     var JavaScript = this.instantiateFromCode('JavaScript', code, false);
 
     this.connect(JavaScript, 'out', 'data');
