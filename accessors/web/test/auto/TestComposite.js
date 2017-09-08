cd378ee80df66799920dbb398d3062c537a4d64c
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-09-08 12:50:14 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- accessors/web/test/auto/TestComposite.js
+++ accessors/web/test/auto/TestComposite.js
@@ -3,9 +3,9 @@ exports.setup = function () {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/test/auto; node ../../node_modules/@accessors-hosts/node/nodeHostInvoke.js test/auto/TestComposite)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
+    //  $PTII/bin/capecode $PTII/./ptolemy/cg/kernel/generic/accessor/test/auto/TestComposite.xml
 
     // Ports: TestComposite: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
     this.input('input', {'type' : 'int', 'value':0});
