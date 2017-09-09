34d1e09c79a8a855ddb394eb187363c4c76878d4
marten <19938940+lhstrh@users.noreply.github.com>
2017-09-09 00:03:53 +0000

    Adding package.json.

--- accessors/web/hosts/cordova/plugins/BLEAdvertise/src/android/BLEAdvertise.java
+++ accessors/web/hosts/cordova/plugins/BLEAdvertise/src/android/BLEAdvertise.java
@@ -1,5 +1,3 @@
-package cordova-plugin-ble-advertise;
-
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
@@ -8,9 +6,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.uriio.beacons.Beacons;
+import com.uriio.beacons.model.Beacon;
+import com.uriio.beacons.model.EddystoneURL;
+
 import java.util.HashMap;
-
-
+import java.util.Map;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -18,22 +18,21 @@ import java.util.HashMap;
 public class BLEAdvertise extends CordovaPlugin {
 
     private HashMap<Integer, Beacon> activeBeacons = new HashMap<Integer, Beacon>();
-    private counter = 0;
+    private int counter = 0;
 
     @Override
     public boolean execute(String action, JSONArray args, 
         CallbackContext callbackContext) throws JSONException {
-        switch (action) {
-            case "initialize":
-                this.initialize();
-                break;
-            case "startEddystoneURLBeacon":
-                String url = args.getString(0);
-                this.startEddystoneURLBeacon();
-                break;
-            default:
-                throw new IllegalArgumentException("No such method: " + action);
+        if (action == "initialize") {
+            this.initialize();
         }
+        else if (action == "startEddystoneURLBeacon") {
+            String url = args.getString(0);
+            this.startEddystoneURLBeacon(url, callbackContext);
+        } else {
+            throw new IllegalArgumentException("No such method: " + action);
+        }
+        
         // FIXME: example code, remove.
         if (action.equals("coolMethod")) {
             String message = args.getString(0);
@@ -55,22 +54,46 @@ public class BLEAdvertise extends CordovaPlugin {
      * Initialize the BLE beacon advertising library.
      */
     private void initialize() {
-        // Don't initialize twice.
-        try {
-            Beacons.getInstance();
-        } catch(RuntimeException e) {
-            Beacons.initialize(this);    
+        if (!Beacons.isInitialized()) {
+            Beacons.initialize(this.cordova.getActivity().getApplicationContext());
         }
     }
 
-     /**
+    /**
      * Initialize the BLE beacon advertising library.
      */
     private void startEddystoneURLBeacon(String url, CallbackContext callbackContext) {
         // FIXME: perhaps just do initialize here, and remove it from the interface.
-        Beacon beacon = new EddystoneURL(url).start();
+        Beacon beacon = new EddystoneURL(url);
         int handle = this.counter++;
-        this.activeBeacons.add(handle, beacon);
-        callbackContext.success(handle);
+        this.activeBeacons.put(handle, beacon);
+        if (beacon.start()) {
+            callbackContext.success(handle);    
+        } else {
+            callbackContext.success("Unable to start advertisement.");
+        }
+        
+    }
+
+    /**
+     * Stop advertisement of particular beacon.
+     */
+    private void stop(String handle, CallbackContext callbackContext) {
+        Beacon beacon = this.activeBeacons.get(handle);
+        if (beacon != null) {
+            beacon.stop();
+            this.activeBeacons.remove(handle);
+        } else {
+            callbackContext.error("No beacon associated with handle: " + handle);
+        }
+    }
+
+    /**
+     * Stop advertisement of all beacons.
+     */
+    private void stop(CallbackContext callbackContext) {
+        for (Map.Entry<Integer, Beacon> entry : this.activeBeacons.entrySet()) {
+            entry.getValue().stop();
+        }
     }
 }
