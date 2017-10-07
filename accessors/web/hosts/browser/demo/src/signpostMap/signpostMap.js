c6793b5d3481695a60804c3dd68ab78a3287df7a
beth <21232697+bethosyk@users.noreply.github.com>
2017-10-07 18:56:11 +0000

    Updated signpost demo.

--- accessors/web/hosts/browser/demo/src/signpostMap/signpostMap.js
+++ accessors/web/hosts/browser/demo/src/signpostMap/signpostMap.js
@@ -1,36 +1,41 @@
-// Construct REST request. 
-var signposts = [ { mac : 'c098e5120003'}];
+// Fetch gps and audio data for each signpost and display on a map.
 
-//  edu.berkeley.eecs.c098e5120003.signpost_gps.v0-0-1
+// Initialize map to sample data.
+var now = Date.now() / 1000;	// Sample data is 30 minutes long, every 10 seconds.
+var sampleX = [];
+var sampleZ = [];
+
+for (var i = 0; i < 180; i++) {
+	sampleX.unshift( (now - i * 10) / 60);
+	sampleZ.unshift( [Math.random()*10 + 45, Math.random()*10 + 45, 
+			Math.random()*10 + 45, Math.random()*10 + 45, 
+			Math.random()*10 + 45, Math.random()*10 + 45, 
+			Math.random()*10 + 45]);
+}
+
+var signposts = [ { mac : 'c098e5120001', xData: sampleX, zData: sampleZ, usingSample: true},
+				  { mac : 'c098e5120003', xData: sampleX, zData: sampleZ, usingSample: true}, 
+				  { mac : 'c098e5120004', xData: sampleX, zData: sampleZ, usingSample: true}];
+
+var mapDrawn = false; 
 
 var gpsIndex = 0;
-var ambientIndex = 0;
-var record = -6;
-
 var gpsUrl = 'edu.berkeley.eecs.' + signposts[0].mac + '.signpost_gps.v0-0-1';
 
 var gpsOptions = {"url":"https://gdp-rest-01.eecs.berkeley.edu/gdp/v1/gcl/" + gpsUrl + "?recno=-1",
 				"headers":{"Authorization":"Basic ZWNkZW1vOnRlcnJhc3dhcm0="},
 				"method":"GET"};
 
-var ambientUrl = 'edu.berkeley.eecs.' + signposts[0].mac + '.signpost_ambient.v0-0-1';
-
-// This only subscribes to one signpost at the moment.
-// Need separate websockets for multiple signposts.
-signposts[0].temps = [];
-
-
 var subscribeTo = {};
-subscribeTo.logname = ambientUrl;
-//subscribeTo.logname = gclPrefix + '.' + mac + '.signpost_audio_frequency.v0-0-1';
-subscribeTo.startrec = -10;	// To get initial data to populate the graph.
-subscribeTo.numrec = 100;
+subscribeTo.startrec = -1000;	// To get initial data to populate the graph.
+subscribeTo.numrec = 0;  
 
 var initialValues = {};
 initialValues["REST.options"] = JSON.stringify(gpsOptions);
 initialValues["REST.trigger"] = true;
 
-// No /wss for ws://
+subscribeTo.logname = 'edu.berkeley.eecs.' + signposts[0].mac + '.signpost_audio_frequency.v0-0-1';
+
 initialValues["WebSocketClient.server"] = 'gdp-rest-01.eecs.berkeley.edu';
 // Use 9007 for ws:// and 9008 for wss://
 initialValues["WebSocketClient.port"] = 9008;
@@ -38,21 +43,21 @@ initialValues["WebSocketClient.toSend"] = JSON.stringify(subscribeTo);
 initialValues["WebSocketClient.sslTls"] = true;
 initialValues["WebSocketClient.trustAll"] = true;
 
-// Assumes library will load before user clicks a marker.
-var sampleGPS = {lat: 37.8732, lng: 122.2576};
-var sampleTemps = [];
-sampleTemps.push({ts: 1506774678, temperature_c: 24.56});
-sampleTemps.push({ts: 1506784678, temperature_c: 24.78});
-sampleTemps.push({ts: 1506794678, temperature_c: 25.34});
-sampleTemps.push({ts: 1506804678, temperature_c: 25.87});
-sampleTemps.push({ts: 1506814678, temperature_c: 26.02});
+subscribeTo.logname = 'edu.berkeley.eecs.' + signposts[1].mac + '.signpost_audio_frequency.v0-0-1';
+initialValues["WebSocketClient2.server"] = 'gdp-rest-01.eecs.berkeley.edu';
+initialValues["WebSocketClient2.port"] = 9008;
+initialValues["WebSocketClient2.toSend"] = JSON.stringify(subscribeTo);
+initialValues["WebSocketClient2.sslTls"] = true;
+initialValues["WebSocketClient2.trustAll"] = true;
 
-google.charts.load('current', {packages: ['corechart', 'line']});
-//google.charts.setOnLoadCallback(function() {
-var mapDrawn = false;
+subscribeTo.logname = 'edu.berkeley.eecs.' + signposts[2].mac + '.signpost_audio_frequency.v0-0-1';
+initialValues["WebSocketClient3.server"] = 'gdp-rest-01.eecs.berkeley.edu';
+initialValues["WebSocketClient3.port"] = 9008;
+initialValues["WebSocketClient3.toSend"] = JSON.stringify(subscribeTo);
+initialValues["WebSocketClient3.sslTls"] = true;
+initialValues["WebSocketClient3.trustAll"] = true;
 
 /** Find the signpost that corresponds to this marker.
- * 
  * @param marker The marker that was clicked on.
  * @returns The signpost associated with that marker, or null if none found.
  */
@@ -106,66 +111,117 @@ function drawMap() {
 	      		// Google charts does not support centering title.  Create separately.
 	      		var title = document.createElement('div');
 	      		title.setAttribute('class', 'title');
-	      		title.innerHTML = marker.mac + ' Temperature &deg;C';
-	      		container.appendChild(title);
+
 	      		
 	      		var node = document.createElement('div');
 	      		node.setAttribute('class', 'chart');
 	      		infoWindow = new google.maps.InfoWindow();
 	            
-	      		// Create the data table.  Save with marker so we can edit it later.
-	      		marker.markerData = new google.visualization.DataTable();
-	      		marker.markerData.addColumn('datetime', 'Time');
-	      		marker.markerData.addColumn('number', 'Temperature');
-	      		
-	      		var dataSource = findSignpost(marker);
-	      		if (dataSource !== null) {
-	      			for (var i = 0; i < dataSource.temps.length; i++) {
-		      			marker.markerData.addRow([new Date(dataSource.temps[i].ts * 1000),
-		      					dataSource.temps[i].temperature_c]);
-		      		}
-		
-		      		// Assumes data is ordered.
-		      		var startDate, endDate;
-		      		if (dataSource.temps.length > 1) {
-		      			startDate = new Date(dataSource.temps[0].ts*1000);
-		      			endDate = new Date(dataSource.temps[dataSource.temps.length - 1].ts*1000);
-		      		}
+	      		var source = findSignpost(marker);
+	      		if (source !== null) {
+	      			if (source.usingSample) {
+	      				title.innerHTML = marker.mac + ' Audio Spectrum Energy (Sample)';
+	      			} else {
+	      				title.innerHTML = marker.mac + ' Audio Spectrum Energy';
+	      			}
 		      		
-		            var options = {
-		            	legend: {position: 'none'},
-		                height: 100,
-		                width: 300,
-		            	hAxis: {
-		                  viewWindow: {
-		                	  min: startDate,
-		                	  max: endDate
-		                    },
-		                  gridlines: {
-		                      count: -1,
-		                      units: {
-		                        days: {format: ['MMM dd']},
-		                        hours: {format: ['HH:mm', 'ha']},
-		                      }
-		                    },
-		                    minorGridlines: {
-		                      units: {
-		                        hours: {format: ['hh:mm:ss a', 'ha']},
-		                        minutes: {format: ['HH:mm a Z', ':mm']}
-		                      }
-		                    }
-		
-		                }
-		            };
-		            
-		            var chart = new google.visualization.LineChart(node);
-		            chart.draw(marker.markerData, options);
+		      		container.appendChild(title);
+	      			
+	    			var logy = [Math.log(63), Math.log(160), Math.log(400), Math.log(1000), Math.log(2500), Math.log(6250), Math.log(16000)];
+	    			
+	    			var relXData = [];
+	    			var xBase = source.xData[0];
+	    			for (var i = 0; i < source.xData.length; i++) {
+	    				relXData.push(source.xData[i] - xBase);
+	    			}
+	    			
+	    			// Z data needs to be in arrays of x times per y frequencies.
+	    			var newZData = [];
+	    			for (var i = 0; i < logy.length; i++) {
+	    				newZData.push([]);
+	    			}
+	    			
+	    			
+	    			for (var i = 0; i < source.zData.length; i++) {
+	    				for(var j = 0; j < logy.length; j++) {
+	    					newZData[j].push(source.zData[i][j]);
+	    				}
+	    			}
+	    			
+					var data = [{
+	            		// x is time, y is frequency, z is amplitude in db.
+	            			   x: relXData,
+	            		       y: [Math.log(63), Math.log(160), Math.log(400), Math.log(1000), Math.log(2500), Math.log(6250), Math.log(16000)],
+	            	           z: newZData,
+	            	           type: 'surface',
+	            	           colorbar: {len: 0.5, thickness: 8, y: 0.7, x: 0.95, tickfont: {size: 10}}
+	            	        }];
+	            	
+	    	        	var layout = {
+    	        			scene: {
+    	        				camera: {
+    	        					up: {x:0, y:0, z:1},
+    	        					center: {x:0.15, y:0, z: -0.5},
+    	        					eye: {x:2, y:2, z:0.8}
+    	        				
+    	        				},
+    		        			xaxis: {
+    		        				range: [relXData[0], 
+    		        					relXData[relXData.length - 1]],
+    		        				tickfont: {
+    		        					size: 10
+    		        				},
+    		        				title: 'Minutes',
+    		        				titlefont: {
+    		        					size: 10
+    		        				}
+    		        			},
+    		        			yaxis: {
+    		        				numticks: 7,
+    		        			    tickvals: [Math.log(63), Math.log(160), Math.log(400), Math.log(1000), Math.log(2500), Math.log(6250), Math.log(16000)],
+    		        			    ticktext: [63, 160, 400, 1000, 2500, 6250, 16000],
+    		        				tickfont: {
+    		        					size: 10
+    		        				},
+    		        				range: [Math.log(63), Math.log(16000)],
+    		        				title: 'Frequency (Hz)',
+    		        				titlefont: {
+    		        					size: 10
+    		        				}
+    		        			},
+    		        			zaxis: {
+    		        				tickfont: {
+    		        					size: 10
+    		        				},
+    		        				title: 'db',
+    		        				titlefont: {
+    		        					size: 10
+    		        				}
+    		        			},
+    	        			},
+      		        	  autosize: false,
+      		        	  width: 450,
+      		        	  height: 300,
+      		        	  margin: {
+      		        	    l: 10,
+      		        	    r: 5,
+      		        	    b: 5,
+      		        	    t: 10,
+      		        	    pad: 4
+      		        	  }
+	          		};
+	      		        	
+	            	Plotly.newPlot(node, data, layout, {displayModeBar: false});
+	      		        
 		            container.appendChild(node);
 		            
 		            infoWindow.setContent(container);      		
 		  			infoWindow.open(map, marker);
+
 	      		} else {
 	      			console.log('Could not find signpost for marker ' + marker.mac);
+      				title.innerHTML = marker.mac + ' Audio Spectrum Energy';
+      				container.appendChild(title);
 	      		}
 	          });
 	      });
@@ -178,8 +234,6 @@ function drawMap() {
  */
 // Called back from .html file once map is loaded.
 function getSignposts() {
-	var gpsReady = false;
-	var tempsReady = false;
 	
 	// Watch for changes in the REST.response output.
 	// Wait a bit after onload to make sure accessors are fully loaded.
@@ -194,6 +248,7 @@ function getSignposts() {
 		  	.addEventListener('DOMSubtreeModified', function() {
 	  		  var value = document.getElementById('REST.response').innerText;
 			  
+	  		  // TODO:  Make three rest calls to fetch all gps data.  Test this first.
 			  var gpsData = JSON.parse(value);
 			  signposts[gpsIndex].lat = gpsData.latitude.value;
 			  signposts[gpsIndex].lng = gpsData.longitude.value;
@@ -208,10 +263,8 @@ function getSignposts() {
 				  document.getElementById('REST.options').value = JSON.stringify(gpsOptions);
 				  reactIfExecutable('REST');
 			  } else {
-				  gpsReady = true;
-				  if (tempsReady) {
-	  				  drawMap();
-				  }
+				// Draw map and assume websocket data will load before person clicks on map.
+	  			drawMap();
 			  }
 		  });
 		  	
@@ -219,75 +272,118 @@ function getSignposts() {
 		  	.addEventListener('DOMSubtreeModified', function() {
 		  		// An error occurred.  Create overlay with default data.
 		  		console.log('Error invoking WebSocketClient accessor.  Using sample data.');
-	  			  // FIXME:  Support multiple signposts.
-	  			  signposts[0].temps = sampleTemps;
-	  			  if (signposts[0].lat === null || typeof signposts[0].lat === 'undefined') {
-	  				  signposts[0].lat = sampleGPS.lat;
-	  				  signposts[0].lng = sampleGPS.lng;
-	  			  }
-	  			  drawMap();
+		  		useSampleData(0);
 		  	});
 		
 		  document.getElementById('WebSocketClient.received')
 		  	.addEventListener('DOMSubtreeModified', function() {
 	  		  var value = document.getElementById('WebSocketClient.received').innerText;
-	  		//console.log('Fetched ambient data ' + value);
-	  		
 	  		// TODO: Try/catch here.
 	  		  var data = JSON.parse(value);
 	  		  if (data.type === 2) {
-	  			  console.log('Error fetching websocket data.  Using sample data.');
-	  			  // TODO:  Support multiple signposts.
-	  			  signposts[0].temps = sampleTemps;
-	  			  if (signposts[0].lat === null || typeof signposts[0].lat === 'undefined') {
-	  				  signposts[0].lat = sampleGPS.lat;
-	  				  signposts[0].lng = sampleGPS.lng;
-	  			  }
-	  			  drawMap();
+	  			 console.log('Error fetching websocket data.  Using sample data.');
+	  			 console.log('Subscription ended.');
 	  		  } else {
-		  		  var datum = {};
-		  		  var tempc = JSON.parse(data.datum.data);
-		  		  datum.temperature_c = tempc.temperature_c.value;
-		  		  datum.ts = data.datum.ts.tv_sec;
-		  		  
-		  		  if (signposts[0].temps.length < 10) {
-		  			  signposts[0].temps.push(datum);
-		  			  if (signposts[0].temps.length === 10) {
-		  				  tempsReady = true;
-		  				  if (gpsReady) {
-		  					drawMap();
-		  				  }
-		  			  }
-		  		  } else {
-		  			  signposts[0].temps.shift();
-		  			  signposts[0].temps.push(datum);
-		  			  
-		  			  // TODO: Update any open info windows
-		  			  
-		  		  }
+	  			 parseData(data, 0);
 	  		  }
 		  });
-
 		  
-		  // Add button listener for show/hide button.
+		  document.getElementById('WebSocketClient2Error')
+		  	.addEventListener('DOMSubtreeModified', function() {
+		  		// An error occurred.  Create overlay with default data.
+		  		console.log('Error invoking WebSocketClient accessor.  Using sample data.');
+		  		useSampleData(1);
+		  	});
+		
+		  document.getElementById('WebSocketClient2.received')
+		  	.addEventListener('DOMSubtreeModified', function() {
+	  		  var value = document.getElementById('WebSocketClient2.received').innerText;
+	  		// TODO: Try/catch here.
+	  		  var data = JSON.parse(value);
+	  		  if (data.type === 2) {
+	  			  console.log('Subscription ended.');
+	  		  } else {
+	  			  parseData(data, 1);
+	  		  }
+		  });
+		  
+		  document.getElementById('WebSocketClient3Error')
+		  	.addEventListener('DOMSubtreeModified', function() {
+		  		// An error occurred.  Create overlay with default data.
+		  		console.log('Error invoking WebSocketClient accessor.  Using sample data.');
+		  		useSampleData(2);
+		  	});
+		
+		  document.getElementById('WebSocketClient3.received')
+		  	.addEventListener('DOMSubtreeModified', function() {
+	  		  var value = document.getElementById('WebSocketClient3.received').innerText;
+	  		// TODO: Try/catch here.
+	  		  var data = JSON.parse(value);
+	  		  if (data.type === 2) {
+	  			console.log('Subscription ended.');
+	  		  } else {
+	  			  parseData(data, 2);
+	  		  }
+		  });
+		 
 		 document.getElementById("showAccessor").addEventListener("click", function() {
-			var accessorDiv = document.getElementById("REST");
-			var accessorDiv2 = document.getElementById("WebSocketClient");
-			
-			if (accessorDiv.style.display === "none") {
-				accessorDiv.style.display = "";
-				accessorDiv2.style.display = "";
-				document.getElementById("showAccessor").textContent = "Hide Accessors";
-			} else {
-				accessorDiv.style.display = "none";
-				accessorDiv2.style.display = "none";
-				document.getElementById("showAccessor").textContent = "Show Accessors";
-			}
-		});
+				var div = document.getElementById("accessorContainer");
+				
+				if (div.style.display === "none") {
+					div.style.display = "";
+					document.getElementById("showAccessor").textContent = "Hide Accessors";
+				} else {
+					div.style.display = "none";
+					document.getElementById("showAccessor").textContent = "Show Accessors";
+				}
+			});
 		 
 		  // React accessor first time.
 		  reactIfExecutable('REST');
 		  reactIfExecutable('WebSocketClient');
-	}, 10000);
+		  reactIfExecutable('WebSocketClient2');
+		  reactIfExecutable('WebSocketClient3');
+	}, 2000);
+}
+
+/** Parse data received from the websocket.
+ * 
+ * @param data The data received from the websocket.
+ * @param i The index of this signpost.
+ */
+function parseData(data, i) {
+	var values = JSON.parse(data.datum.data);
+	// Time in seconds.  Check for duplicates; only save one per second.
+	var time = Math.floor(new Date(values._meta.received_time) / 1000);
+	if (signposts[i].usingSample === true) {
+		signposts[i].usingSample = false;
+		signposts[i].xData = [];
+		signposts[i].zData = [];
+	}
 	
+	if (signposts[i].xData.length === 0 || 
+			signposts[i].xData[signposts[i].xData.length -1] !== time) {
+		var z = [values['63Hz'].value, values['160Hz'].value, 
+			values['400Hz'].value, values['1000Hz'].value, 
+			values['2500Hz'].value, values['6250Hz'].value, 
+			values['16000Hz'].value];
+		
+		if (signposts[i].xData.length > (-subscribeTo.startrec) - 100) {
+			signposts[i].xData.shift();
+			signposts[i].zData.shift();
+		}
+
+		signposts[i].xData.push(time/60);	// Time in minutes.
+		signposts[i].zData.push(z);
+	}
+}
+
+/** Use sample data for the given signpost.
+ * 
+ * @param i  The index of the signpost.
+ */
+function useSampleData(i) {
+	signposts[i].xData = sampleX;
+	signposts[i].zData = sampleZ;
+	signposts[i].usingSample = true;
 }
