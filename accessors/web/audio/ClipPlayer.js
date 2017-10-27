4c34a5e0b00d29d1454464e096fcd08c7ee205b5
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-10-27 22:48:25 +0000

    getResource() now handles a returnURI option that, if true, returns the URI of the resource instead of the contents of the resource itself.  This is used by ClipPlayer to pass a URI to the JavaFX MediaPlayer so that we can support mp3 files and also have the semantics of getResource() when searching for resources.

--- accessors/web/audio/ClipPlayer.js
+++ accessors/web/audio/ClipPlayer.js
@@ -69,7 +69,11 @@ exports.initialize = function () {
             if (self.player !== null) {
                 self.player.stop();        // audio.js checks if it's actually playing.
             }
-            self.player = new audio.ClipPlayer(url);
+            // Use getResource() to check for illegal URIs.  Note that
+            // getResource() does not support file: as an argument,
+            // but may return a valid URI that starts with file://
+            var resourceURI = getResource(url, {'returnURI' : 'true'});
+            self.player = new audio.ClipPlayer(resourceURI);
             self.previousURL = url;
             
             self.player.on('done', function() {
