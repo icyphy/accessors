dc8ef53a87b106fd2a099cb366750164dd3d5f24
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-11-24 17:04:01 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- accessors/web/cameras/demo/CaptureOpenLoop.js
+++ accessors/web/cameras/demo/CaptureOpenLoop.js
@@ -3,9 +3,9 @@ exports.setup = function () {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/cameras/demo; @node@ ../../node_modules/@accessors-hosts/node/nodeHostInvoke.js cameras/demo/CaptureOpenLoop)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Camera/CaptureOpenLoop.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./ptolemy/actor/lib/jjs/modules/cameras/demo/Camera/CaptureOpenLoop.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/ptolemy/actor/lib/jjs/modules/cameras/demo/Camera/CaptureOpenLoop.xml
+    //  $PTII/bin/capecode $PTII/./ptolemy/actor/lib/jjs/modules/cameras/demo/Camera/CaptureOpenLoop.xml
 
     // Ports: CaptureOpenLoop: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
