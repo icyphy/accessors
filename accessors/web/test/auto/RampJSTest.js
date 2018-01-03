badc84034a56b7700e039f8490e95e4125c635a1
Christopher Brooks <cxh@eecs.berkeley.edu>
2018-01-03 15:07:43 +0000

    Reloaded accessors using /home/jenkins/workspace/ptII/bin/reloadAllAccessors.

--- accessors/web/test/auto/RampJSTest.js
+++ accessors/web/test/auto/RampJSTest.js
@@ -3,9 +3,9 @@ exports.setup = function () {
     //  To run the code, run: 
     //  (cd $PTII/org/terraswarm/accessor/accessors/web/test/auto; @node@ ../../node_modules/@accessors-hosts/node/nodeHostInvoke.js test/auto/RampJSTest)
     //  To regenerate this composite accessor, run:
-    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTest.xml
+    //  $PTII/bin/ptinvoke ptolemy.cg.kernel.generic.accessor.AccessorCodeGenerator -language accessor $PTII/./ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTest.xml
     //  to edit the model, run:
-    //  $PTII/bin/capecode $PTII/ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTest.xml
+    //  $PTII/bin/capecode $PTII/./ptolemy/cg/kernel/generic/accessor/test/auto/RampJSTest.xml
 
     // Ports: RampJSTest: ptolemy/cg/adapter/generic/accessor/adapters/ptolemy/actor/TypedCompositeActor.java
 
