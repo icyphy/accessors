c51184301e1e51e8268422a0ab65c77e29df8f39
mew2ub <matt.weber@berkeley.edu>
2018-06-19 20:14:06 -0700

    Renamed SemanticRepository accessor to SemanticRepositoryQuery and added a new SemanticRepositoryUpdate accessor

--- web/services/SemanticRepositoryUpdate.js
+++ web/services/SemanticRepositoryUpdate.js
@@ -1,4 +1,4 @@
-/** Perform a SPARQL query on a semantic repository such as GraphDB.
+/** Perform a SPARQL update on a semantic repository such as GraphDB.
  * 
  *  SPARQL is a W3C standard for querying semantic repositories,
  *  playing an analogous role to SQL with respect to relational
@@ -7,27 +7,31 @@
  *  entries in the repository have the form "subject", "predicate", "object"
  *  eg. "An Apple" "IsA" "Fruit"
  *  
- *  An example SPARQL query to get a list of up to 100 triples in the repository:
+ *  An example SPARQL update to add a triple to the repository:
  *
- *  select * where { 
- *      ?s ?p ?o .
- *  } limit 100
+ *  INSERT DATA {
+ *      <http://ptolemy.berkeley.edu/a> <http://ptolemy.berkeley.edu/b> <http://ptolemy.berkeley.edu/c> .
+ *  }
  *
- *  Documentation for SPARQL and the SPARQL Protocol can be found at
- *  https://www.w3.org/TR/rdf-sparql-query/
+ *  Documentation for SPARQL and the SPARQL Protocol 1.1 introducing updates can be found at
+ *  https://www.w3.org/TR/sparql11-query/
  *
  *  Information on GraphDB can be found at
  *  http://graphdb.ontotext.com/
  *  
  *  SPARQL is also a protocol for communicating with RDF databases
- *  Upon receiving a query input, this accessor performs an http GET
- *  to the specified server and port with the querystring "query" set to the
- *  query input. The JSON response from the semantic repository (if any) is 
- *  sent to the response output. Depending on the query type, the 
- *  response will be JSON 
+ *  Upon receiving an update input, this accessor performs an http POST
+ *  to the specified server and port with the body set to the
+ *  update input. Upon receiving a 204 response from the server, this
+ *  accessor outputs the string "success". If a different response is
+ *  received this accessor throws an error (producing no output). 
+ *  Note that an INSERT that inserts data already in the repository or
+ *  a DELETE that removes data already absent from the 
+ *  repository is treated by the respository as a
+ *  successful update.
  *
  *  This accessor does not block waiting for the response, but if any additional
- *  *query* input is received before a pending request has received a response
+ *  *update* input is received before a pending request has received a response
  *  or timed out, then the new request will be queued and sent out only after
  *  the pending request has completed. This strategy ensures that outputs are
  *  produced in the same order as the input requests.
@@ -35,17 +39,15 @@
  *  @accessor services/SemanticRepository
  *  @author Matt Weber
  *  @version $$Id: SemanticRepository.js 1725 2017-05-19 22:59:11Z cxh $$
- *  @input {string} query The SPARQL query to be sent to the semantic repository.
- *   Types of queries yielding a response are: SELECT, CONSTRUCT, ASK, and DESCRIBE
+ *  @input {string} update The SPARQL update to be sent to the semantic repository.
+ *   Types of updates are: INSERT and DELETE
  *  @parameter {string} host The URL for the semantic repository.
  *  @parameter {string} port The port for the semantic repository.
  *  @parameter {string} repositoryName The name of the particular repository on the host.
  *  @parameter {int} timeout The amount of time (in milliseconds) to wait for a response
  *   before triggering a null response and an error. This defaults to 20000.
- *  @output response An object containing the raw response from the service conforming
- *   to the SPARQL protocol. If the query type was SELECT or ASK the response will be
- *   JSON "sparql-results". If the query type was CONSTRUCT or DESCRIBE the response
- *   will be JSON "rdf".
+ *  @output response The string "success" if this accessor gets a 204 status code
+ *   back from the semantic repository. Otherwise no output is produced.
  */
 
 
@@ -55,12 +57,12 @@
 /*globals addInputHandler, addInputParameter, console, error, exports, extend, input, get, getParameter, getResource, output, parameter, send */
 /*jshint globalstrict: true*/
 'use strict';
- 
+
  /** Set up the accessor by defining the inputs and outputs.
  */
 exports.setup = function () {
     this.extend('net/REST');
-    this.input('query', {
+    this.input('update', {
         'type': 'string'
     });
 
@@ -123,34 +125,43 @@ exports.filterResponse = function(response){
 //Overriding REST
 //Connections to the SemanticRepository should be closed once data has been received.
 exports.handleResponse = function(message){
-     exports.ssuper.handleResponse.call(this, message);
-     exports.ssuper.wrapup();
+    if(message.statusCode == 204){
+        this.send('response', 'Success')
+        exports.ssuper.wrapup();
+    } else {
+        exports.ssuper.wrapup();
+        error('Received a ' + message.statusCode + ' status code from SemanticRepository. 204 indicates success.');
+    }
 }
 
 exports.initialize = function(){
     exports.ssuper.initialize.call(this);
     var thiz = this;
 
-    this.addInputHandler('query', function(){
-        var queryInput = thiz.get('query');
+    this.addInputHandler('update', function(){
+        var updateInput = thiz.get('update');
         var host = thiz.getParameter('host');
         var port = thiz.getParameter('port');
         var repositoryName = thiz.getParameter('repositoryName');
 
         var options = {
-            'headers' : {'Accept':'application/sparql-results+json, application/rdf+json'},
-            'method'  : 'GET',
+            'headers' : {//'Accept':'application/json',
+                        'Content-Type': 'application/sparql-update'
+                        //'Content-Type': 'application/x-www-form-urlencode'
+                        },
+            'method'  : 'POST',
             'url'     : {'host'  : host,
-                        'port'   : port,
+                        'port'   : port
                         }
         };
 
-        var command = 'repositories/' + repositoryName;
-        var args = {'query' : queryInput} ;
+        var command = 'repositories/' + repositoryName + '/statements';
+
+        var body = updateInput;
 
         thiz.send('options', options);
         thiz.send('command', command);
-        thiz.send('arguments', args);
+        thiz.send('body', body);
         thiz.send('trigger', true);
     });
 };
