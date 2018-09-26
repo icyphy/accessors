f6ac3e1252c3bb38761f95e27b3045abf6a91240
mew2ub <matt.weber@berkeley.edu>
2018-09-25 17:39:30 -0700

    Semantic Repostitory query accessors can now be used for user authentication over https

--- web/services/SemanticRepositoryQuery.js
+++ web/services/SemanticRepositoryQuery.js
@@ -37,9 +37,17 @@
  *  @version $$Id: SemanticRepository.js 1725 2017-05-19 22:59:11Z cxh $$
  *  @input {string} query The SPARQL query to be sent to the semantic repository.
  *   Types of queries yielding a response are: SELECT, CONSTRUCT, ASK, and DESCRIBE
+ *  @parameter {string} protocol Either http or https.
  *  @parameter {string} host The URL for the semantic repository.
  *  @parameter {string} port The port for the semantic repository.
  *  @parameter {string} repositoryName The name of the particular repository on the host.
+ *  @parameter {boolean} authenticate If true, enable authentication to an access controlled
+ *   semantic repository by sending username and password with request. If false, username 
+ *   and password information will not be sent. An error will occur if the http protocol is
+ *   selected with a true authenticate setting to avoid sending username/password information
+ *   in plain text.
+ *  @parameter {string} username A username for an access controlled semantic repository. 
+ *  @parameter {string} password A password for an access controlled semantic repository.
  *  @parameter {int} timeout The amount of time (in milliseconds) to wait for a response
  *   before triggering a null response and an error. This defaults to 20000.
  *  @output response An object containing the raw response from the service conforming
@@ -58,10 +66,19 @@
  
  /** Set up the accessor by defining the inputs and outputs.
  */
+
+var base64 = require('base64-js');
+
 exports.setup = function () {
     this.extend('net/REST');
     this.input('query', {
         'type': 'string'
+    });
+
+    this.parameter('protocol', {
+        'type': 'string',
+        'value': 'http',
+        'options': ['http', 'https']
     });
 
     this.parameter('host', {
@@ -76,6 +93,21 @@ exports.setup = function () {
 
     this.parameter('repositoryName', {
         'type': 'string',
+    });
+
+    this.parameter('authenticate', {
+        'type': 'boolean',
+        'value': false
+    });
+
+    this.parameter('username', {
+        'type': 'string',
+        'value': 'admin'
+    });
+
+    this.parameter('password', {
+        'type': 'string',
+        'value': 'root'
     });
 
     //Overriding inherited default timeout value of 5000ms to allow for longer queries by default
@@ -118,17 +150,23 @@ exports.setup = function () {
 //Overriding REST
 exports.filterResponse = function(response){
     return JSON.parse(response);
-}
+};
 
 //Overriding REST
 //Connections to the SemanticRepository should be closed once data has been received.
 exports.handleResponse = function(message){
      exports.ssuper.handleResponse.call(this, message);
      exports.ssuper.wrapup();
-}
+};
 
 exports.initialize = function(){
     exports.ssuper.initialize.call(this);
+    
+    //Check for bad authentication and protocol settings.
+    if(this.getParameter('protocol') == 'http' && this.getParameter('authenticate') ){
+        error("Semantic Repository authentication setting incompatible with protocol setting. This accessor will not send username and password information in plain text over http. Change to https or dissable authentication.");
+    }
+
     var thiz = this;
 
     this.addInputHandler('query', function(){
@@ -136,14 +174,34 @@ exports.initialize = function(){
         var host = thiz.getParameter('host');
         var port = thiz.getParameter('port');
         var repositoryName = thiz.getParameter('repositoryName');
+        var authenticate = thiz.getParameter('authenticate');
+        var protocol = thiz.getParameter('protocol');
 
         var options = {
             'headers' : {'Accept':'application/sparql-results+json, application/rdf+json'},
             'method'  : 'GET',
             'url'     : {'host'  : host,
                         'port'   : port,
+                        'protocol' : protocol
                         }
         };
+
+        //If authenticating, add base64 encoded username and password to headers.
+        //See basic authentication under http://graphdb.ontotext.com/documentation/standard/authentication.html
+        if(authenticate && protocol == 'https'){
+            var username = thiz.getParameter('username');
+            var password = thiz.getParameter('password');
+
+            //Note, contrary to the graphDB documentation, the separator between
+            //username and password is a colon, not a forward slash.
+            var login = username +":" + password;
+            var loginArray = login.split("");
+            var loginNumeric = loginArray.map(function(x){ return x.charCodeAt(0)});
+            var loginUint = new Uint8Array(loginNumeric);
+            var login64 = base64.fromByteArray(loginUint);
+            options.headers.Authorization =  'Basic ' + login64;
+            console.log(options);
+        }
 
         var command = 'repositories/' + repositoryName;
         var args = {'query' : queryInput} ;
