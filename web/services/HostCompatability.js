cf8f650a3a9d48b20e03e9a64a115411f4f0ded8
mew2ub <matt.weber@berkeley.edu>
2018-10-10 18:30:20 -0700

    added default parameter setting for HostCompatability for a special value signifying the host should be determined via the getHostName function. Also improved the accessor's query to determine whether or not the accessor we are testing exists in the ontology. If it doesn't (determined by testing whether the accessor is typed as an interface - which should be inferred from any triples in the repo at all) an error is generated.

--- web/services/HostCompatability.js
+++ web/services/HostCompatability.js
@@ -16,6 +16,8 @@
  *  @input {string} accessorName The full name of the accessor (including the directory 
  *  and .js at the end). Eg. Services/HostCompatability.js.
  *  @parameter {string} accessorHostName The name of the accessor host to be compared with accessorName.
+ *   The special value 'ThisHost' will cause this accessor to use the getHostName function to determine
+ *   the name of the accessor host for comparison.
  *  @parameter {string} host The URL for the semantic repository.
  *  @parameter {string} port The port for the semantic repository.
  *  @parameter {string} repositoryName The name of the particular repository on the host.
@@ -48,8 +50,8 @@ exports.setup = function () {
 
     this.parameter('accessorHostName', {
         'type': 'string',
-        'options': ['Browser', 'Cordova', 'Duktape', 'Nashorn', 'Node'],
-        'value': 'Browser'
+        'options': ['ThisHost', 'Browser', 'Cordova', 'Duktape', 'Nashorn', 'Node'],
+        'value': 'ThisHost'
     });
 
     //Overriding the response output and query input to be hidden
@@ -69,8 +71,16 @@ exports.filterResponse = function(response){
     var data = JSON.parse(response);
     var hostModules = [];
     var accessorModules = [];
+    var typeFound = false;
+
     if(data && data.results && data.results.bindings){
         var dataArray = data.results.bindings;
+        
+        //If the accessor doesn't have the type "Interface"
+        //after inference, it must not appear in any triples and is unknown
+        //in the ontology. This is an invalid query.
+
+        //Sort responses by category (accessor module, host module, type of the so called accessor)
         dataArray.map(function(x){
             if(x.host && x.host.value){
                 hostModules.push(x.host.value);
@@ -78,7 +88,15 @@ exports.filterResponse = function(response){
             if(x.acc && x.acc.value){
                 accessorModules.push(x.acc.value);
             }
+            if(x.type && x.type.value === "http://ptolemy.berkeley.edu/accessors#Interface"){
+                typeFound = true;
+            }
         });
+
+        if(!typeFound){
+            error("Specified accessor doesn't exist in the ontology. An accessor with any triples at all should have type 'Interface' (by inference), and this one doesn't.");
+        }
+
         hostModules.sort();
         accessorModules.sort();
 
@@ -92,7 +110,12 @@ exports.filterResponse = function(response){
             }
         }
         this.send('compatible', match);
+    } else{
+        //If the query failed entirely send an error and report incompatible
+        error("HostCompatability: Query to Semantic Repository failed. Outputing false compatibility.");
+        this.send('compatible', false);
     }
+
 
     //The hidden response output still needs the raw json data to be returned.
     return data;
@@ -106,15 +129,20 @@ exports.initialize = function(){
     this.addInputHandler('accessorName', function(){
         var accName = thiz.get('accessorName');
         var accHostName = thiz.getParameter('accessorHostName');
+        if(accHostName === 'ThisHost'){
+            accHostName = getHostName();
+        }
         
         var constructedQuery = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                                 "PREFIX acc: <http://ptolemy.berkeley.edu/accessors#>" +
                                 "PREFIX host: <http://ptolemy.berkeley.edu/hosts#>" +
-                                "select distinct ?host ?acc where {" +
-                                    "{ {{host:Host.Common host:Implements ?host} UNION {host:Host." + accHostName + " host:Implements ?host}}" +  
+                                "select distinct ?host ?acc ?type where {" +
+                                    "{{{host:Host.Common host:Implements ?host} UNION {host:Host." + accHostName + " host:Implements ?host}}" +  
                                     "{<http://ptolemy.berkeley.edu/accessors/" + accName + "> acc:Requires ?host} } UNION " +
-                                    "{<http://ptolemy.berkeley.edu/accessors/" + accName + "> acc:Requires ?acc}" + 
+                                    "{<http://ptolemy.berkeley.edu/accessors/" + accName + "> acc:Requires ?acc} UNION" +
+                                    "{<http://ptolemy.berkeley.edu/accessors/" + accName + "> rdf:type ?type}" + 
                                 "}";
+
         thiz.send('query', constructedQuery);
     });
 };
