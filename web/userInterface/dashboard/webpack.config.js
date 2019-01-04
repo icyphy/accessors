26a58300544f44be1bd360081590593f746e06ca
mew2ub <matt.weber@berkeley.edu>
2019-01-04 12:35:47 -0800

    Proof of concept for webcomponent accessor user interface

---
+++ web/userInterface/dashboard/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|png|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules:[ path.resolve( __dirname,'src') , 'node_modules'],
+    extensions: ['*', '.js', '.jsx', ".css"]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+};
