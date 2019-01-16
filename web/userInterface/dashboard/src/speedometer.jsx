c7f95e3927049c4fadf3901ce090369f2ca6c9c5
mew2ub <matt.weber@berkeley.edu>
2019-01-15 16:01:48 -0800

    improved implementation of speedometer.jsx to better use React control of state

--- web/userInterface/dashboard/src/speedometer.jsx
+++ web/userInterface/dashboard/src/speedometer.jsx
@@ -7,6 +7,42 @@ import ReactSpeedometer from "react-d3-speedometer"
 //shadow DOM
 import retargetEvents from 'react-shadow-dom-retarget-events';
 
+class SpeedometerControler extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      speed: 0
+    };
+  }
+
+  componentDidMount() {
+    var thiz = this;
+
+    setInterval(function(){ 
+      thiz.setState({speed: 55});
+      console.log("setting to 55");
+     }, 1000);
+    
+    setTimeout( setInterval( function(){
+      thiz.setState({speed: 57});
+      console.log("setting to 57");
+    }, 1500), 1000);
+
+  }
+
+  render(){
+    return (
+    <ReactSpeedometer
+            minValue={0}
+            maxValue={170}
+            value={this.state.speed}
+          />
+          )
+  }
+
+}
+
+
 class SpeedometerComponent extends HTMLElement {
 
   connectedCallback() {
@@ -16,11 +52,7 @@ class SpeedometerComponent extends HTMLElement {
     ReactDOM.render(
         (
         <div>
-          <ReactSpeedometer
-            minValue={0}
-            maxValue={170}
-            value={56}
-          />
+          <SpeedometerControler/>
         </div>
         )
         , mountPoint);
