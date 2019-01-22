fa5e2de744bc6680321a21700c5ca5fe8e575132
mew2ub <matt.weber@berkeley.edu>
2019-01-21 22:31:36 -0800

    Updated interface used by ParkingDialogue to communicate with web app over the websocket

--- web/dashboard/ParkingDialogue.js
+++ web/dashboard/ParkingDialogue.js
@@ -13,9 +13,43 @@ exports.initialize = function(){
     this.addInputHandler("parkingMessage", function(){
         var resourceValue = this.getParameter('MapsAPIKeyLocation');
         var resourceContents = getResource(resourceValue, options, null);
+        
+        //Center for map location (this example is the middle of Berkeley)
+        var exampleLocation = {
+            "lat": 37.8716,
+            "lng": -122.2727,
+        }
+        var exampleData = [
+            {
+                "displayName": "Parking1" ,
+                "lat": 37.8626,
+                "lng": -122.2637,
+                "address": "1234 Place St.",
+                "price": 3.12,
+                "distance": 1.78,
+            },
+            {
+                "displayName": "Parking2" ,
+                "lat": 37.8731,
+                "lng": -122.2617,
+                "address": "4321 Street Pl.",
+                "price": 2.98,
+                "distance": 1.96,
+            },
+                {
+                "displayName": "Parking3" ,
+                "lat": 37.8831,
+                "lng": -122.2517,
+                "address": "9273 Atom Rd.",
+                "price": 3.01,
+                "distance": 2.12,
+            },
+        ];
         var update = {
             "id": "parking",
-            "apiKey": resourceContents
+            "apiKey": resourceContents,
+            "mapPosition": exampleLocation,
+            "parkingData": exampleData
         };
         this.send('response', update);
     });
