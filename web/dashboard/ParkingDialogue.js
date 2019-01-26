3b1ad74509fce81b41a87a0fcd9743d304d91877
mew2ub <matt.weber@berkeley.edu>
2019-01-25 22:47:01 -0800

    Combined the lower half of Ravi's parking ontology demo with the componentDemo controller

--- web/dashboard/ParkingDialogue.js
+++ web/dashboard/ParkingDialogue.js
@@ -1,56 +1,33 @@
 exports.setup = function(){
     this.input("parkingMessage");
-    this.output("response");
-    this.parameter("MapsAPIKeyLocation", {
-        type: "string",
-        value: "MAPS_API_KEY_DONT_COMMIT.txt"
+    this.parameter('searchTerm', {
+        'type': 'string',
+        'value': 'parking',
+        'options': ['parking', 'trafficAlerts', 'drivingAssistance', 'restaurants', 'automotive']
+    });
+    this.output('latitude',{
+        'type': 'number'
+    });
+    this.output('longitude',{
+        'type': 'number'
+    });
+    this.output('queryTerm',{
+        'type': 'string'
     });
 }
 
 exports.initialize = function(){
-    var options = '';
-    var self = this;
+    var thiz = this;
     this.addInputHandler("parkingMessage", function(){
-        var resourceValue = this.getParameter('MapsAPIKeyLocation');
-        var resourceContents = getResource(resourceValue, options, null);
         
-        //Center for map location (this example is the middle of Berkeley)
-        var exampleLocation = {
-            "lat": 37.8716,
-            "lng": -122.2727,
+        //Center for map and query location (this example is the middle of Berkeley)
+        var location = {
+            "lat": 37.8616,
+            "lng": -122.2627,
         }
-        var exampleData = [
-            {
-                "displayName": "Parking1" ,
-                "lat": 37.8626,
-                "lng": -122.2637,
-                "address": "1234 Place St.",
-                "price": 3.12,
-                "distance": 1.78,
-            },
-            {
-                "displayName": "Parking2" ,
-                "lat": 37.8731,
-                "lng": -122.2617,
-                "address": "4321 Street Pl.",
-                "price": 2.98,
-                "distance": 1.96,
-            },
-                {
-                "displayName": "Parking3" ,
-                "lat": 37.8831,
-                "lng": -122.2517,
-                "address": "9273 Atom Rd.",
-                "price": 3.01,
-                "distance": 2.12,
-            },
-        ];
-        var update = {
-            "id": "parking",
-            "apiKey": resourceContents,
-            "mapPosition": exampleLocation,
-            "parkingData": exampleData
-        };
-        this.send('response', update);
+
+        thiz.send('latitude', location.lat);
+        thiz.send('longitude', location.lng);
+        thiz.send('queryTerm', thiz.getParameter('searchTerm'));
     });
 }
