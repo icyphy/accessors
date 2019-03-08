e9d7a858b1af626b7796aea31b19ba33c07d02a0
mew2ub <matt.weber@berkeley.edu>
2019-03-08 13:49:37 -0800

    cleaned up print statements

--- web/hosts/cordova/demo/dashboardTest/src/www/js/index.js
+++ web/hosts/cordova/demo/dashboardTest/src/www/js/index.js
@@ -3,7 +3,6 @@ var app = {
     // Register an event handler that is invoked when the device is ready.
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        console.log("hi from initialize");
     },
 
     // Start swarmlet when the device is ready.
@@ -11,8 +10,6 @@ var app = {
         var topLevel = instantiateAccessor('MyTopLevel', 'Swarmlet', getAccessorCode);
         topLevel.initialize();
         this.updateStatus('Executing'); // FIXME: handle wrap up
-        console.log("hi from onDeviceReady");
-        document.getElementById('root').innerHTML += "hello world! via root selection"
     },
 
     // Update Status
